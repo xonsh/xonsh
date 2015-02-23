f6af8faa34386b941b58c75a414a11b6307f8ef7
Anthony Scopatz <scopatz@gmail.com>
2015-02-22 22:50:17 -0600

    possible regexpath

--- xonsh/built_ins.py
+++ xonsh/built_ins.py
@@ -4,12 +4,14 @@ not to be confused with the special Python builtins module.
 import os
 import builtins
 import subprocess
+from glob import glob, iglob
 from contextlib import contextmanager
 from collections import MutableMapping, Iterable
 
 from xonsh.tools import string_types
 
 BUILTINS_LOADED = False
+ENV = None
 
 class Env(MutableMapping):
     """A xonsh environment, whose variables have limited typing 
@@ -29,8 +31,11 @@ class Env(MutableMapping):
             args = (os.environ,)
         for key, val in dict(*args, **kwargs).items():
             self[key] = val
+        self._detyped = None
 
     def detype(self):
+        if self._detyped is not None:
+            return self._detyped
         ctx = {}
         for key, val in self._d.items():
             if not isinstance(key, string_types):
@@ -40,7 +45,15 @@ class Env(MutableMapping):
             elif not isinstance(val, string_types):
                 val = str(val)
             ctx[key] = val
+        self._detyped = ctx
         return ctx
+
+    def replace_env(self):
+        """Replaces the contents of os.environ with a detyped version 
+        of the xonsh environement.
+        """
+        os.environ.clear()
+        os.environ.update(self.detype())
 
     #
     # Mutable mapping interface
@@ -54,9 +67,11 @@ class Env(MutableMapping):
             val = val.split(os.pathsep) if isinstance(val, string_types) \
                   else val
         self._d[key] = val
+        self._detyped = None
         
     def __delitem__(self, key):
         del self._d[key]
+        self._detyped = None
 
     def __iter__(self):
         yield from self._d
@@ -71,21 +86,68 @@ class Env(MutableMapping):
         return '{0}.{1}({2})'.format(self.__class__.__module__, 
                                      self.__class__.__name__, self._d)
 
-def xonsh_help(x):
-    """A variable to print help about, and then return."""
+def helper(x):
+    """Prints help about, and then returns that variable."""
     
     return x
+
+
+def _partsjoin(parts):
+    s = os.path.join(parts)
+    if len(parts[0]) == 0:
+        s = os.sep + s  # fix for root dir
+    return s
+
+
+def reglob(path, parts=None, i=None):
+    """Regular expression-based globbing."""
+    if parts is None:
+        parts = path.split(os.sep)
+        d = os.sep if path.startswith(os.sep) else '.'
+        return reglob(d, parts=parts, i=0)
+    base = subdir = path
+    if i == 0 and base == '.':
+        base = ''
+    regex = re.compile(os.path.join(base, parts[i]))
+    files = os.listdir(subdir)
+    files.sort()
+    paths = []
+    i1 = i + 1
+    if i1 == len(parts):
+        for f in files: 
+            p = os.path.join(base, f)
+            if regex.match(p) is not None:
+                paths.append(p)
+    else:
+        for f in files: 
+            p = os.path.join(base, f)
+            if regex.match(p) is None or not os.path.isdir(p):
+                continue
+            paths += reglob(p, parts=parts, i=i1)
+    return paths
+
+
+def regexpath(s):
+    """Takes a regular expression string and returns a list of file
+    paths that match the regex.
+    """
+    global ENV
+    if ENV is not None:
+        ENV.replace_env()
+    s = os.path.expanduser(s)
+    s = os.path.expandvars(s)
+    return reglob(s)
 
 
 def load_builtins():
     """Loads the xonsh builtins into the Python builtins. Sets the
     BUILTINS_LOADED variable to True.
     """
-    global BUILTINS_LOADED
-    builtins.__xonsh_env__ = Env()
-    builtins.__xonsh_help__ = lambda x: x
+    global BUILTINS_LOADED, ENV
+    builtins.__xonsh_env__ = ENV = Env()
+    builtins.__xonsh_help__ = helper
     builtins.__xonsh_superhelp__ = lambda x: x
-    builtins.__xonsh_regexpath__ = lambda x: []
+    builtins.__xonsh_regexpath__ = regexpath
     builtins.__xonsh_subproc__ = subprocess
     BUILTINS_LOADED = True
 
@@ -93,7 +155,9 @@ def unload_builtins():
     """Removes the xonsh builtins from the Python builins, if the 
     BUILTINS_LOADED is True, sets BUILTINS_LOADED to False, and returns.
     """
-    global BUILTINS_LOADED
+    global BUILTINS_LOADED, ENV
+    if ENV is not None:
+        ENV = None
     if not BUILTINS_LOADED:
         return
     del (builtins.__xonsh_env__,
