8b3c86905fa88cd3ae7dd57e4c9fc095ac66352b
Anthony Scopatz <scopatz@gmail.com>
2015-03-02 23:01:43 -0600

    added completer

--- xonsh/completer.py
+++ xonsh/completer.py
@@ -1,10 +1,13 @@
 """A (tab-)completer for xonsh."""
 from __future__ import print_function, unicode_literals
 import os
+import re
 import sys
 import builtins
 import subprocess
 from glob import glob, iglob
+
+RE_DASHF = re.compile('-F\s+(\w+)')
 
 XONSH_TOKENS = {'and ', 'as ', 'assert ', 'break', 'class ', 'continue', 
     'def ', 'del ', 'elif ', 'else', 'except ', 'finally:', 'for ', 'from ', 
@@ -17,6 +20,16 @@ XONSH_TOKENS = {'and ', 'as ', 'assert ', 'break', 'class ', 'continue',
 
 class Completer(object):
     """This provides a list of optional completions for the xonsh shell."""
+
+    def __init__(self):
+        try:
+            # FIXME this could be threaded for faster startup times
+            self._load_bash_complete_funcs()
+            # or we could make this lazy
+            self._load_bash_complete_files()
+            self.have_bash = True
+        except subprocess.CalledProcessError:
+            self.have_bash = False
 
     def complete(self, prefix, line, begidx, endidx, ctx=None):
         """Complete the string s, given a possible execution context.
@@ -50,4 +63,52 @@ class Completer(object):
             key = prefix[1:]
             rtn |= {'$'+k for k in builtins.__xonsh_env__ if k.startswith(key)}
         rtn |= {s + (slash if os.path.isdir(s) else space) for s in iglob(prefix + '*')}
+        if begidx == 0:
+            rtn |= self.cmd_complete(prefix)
         return sorted(rtn)
+
+    def cmd_complete(self, cmd):
+        path = builtins.__xonsh_env__.get('PATH', None)
+        if path is None:
+            return set()
+        cmds = set()
+        for d in path:
+            print(d)
+            cmds |= {s for s in os.listdir(d) if s.startswith(cmd)}
+        return cmds
+
+    def _load_bash_complete_funcs(self):
+        input = 'source /etc/bash_completion\n'
+        if os.path.isfile('/usr/share/bash-completion/completions/git'):
+            input += 'source /usr/share/bash-completion/completions/git\n'
+        input += 'complete -p\n'
+        out = subprocess.check_output(['bash'], input=input, 
+                                      universal_newlines=True)
+        self.bash_complete_funcs = bcf = {}
+        for line in out.splitlines():
+            head, cmd = line.rsplit(' ', 1)
+            if len(cmd) == 0:
+                continue
+            m = RE_DASHF.search(head)
+            if m is None:
+                continue
+            bcf[cmd] = m.group(1)
+
+    def _load_bash_complete_files(self):
+        declare_f = 'declare -F '
+        input = ['source /etc/bash_completion']
+        if os.path.isfile('/usr/share/bash-completion/completions/git'):
+            input.append('source /usr/share/bash-completion/completions/git')
+        input.append('shopt -s extdebug')
+        input += [declare_f + f for f in self.bash_complete_funcs.values()]
+        input.append('shopt -u extdebug\n')
+        input = '\n'.join(input)
+        out = subprocess.check_output(['bash'], input=input,
+                                      universal_newlines=True)
+        func_files = {}
+        for line in out.splitlines():
+            parts = line.split()
+            func_files[parts[0]] = parts[-1]
+        self.bash_complete_files = {cmd: func_files[func] for cmd, func in 
+                                    self.bash_complete_funcs.items()
+                                    if func in func_files}
