0f9c7bc1949b59007f030b36f0e605f8738c92e1
James Elías <jamiees2@gmail.com>
2015-03-10 09:43:20 +0000

    Improved output redirection

--- xonsh/tools.py
+++ xonsh/tools.py
@@ -128,3 +128,43 @@ TERM_COLORS = {
     'BACKGROUND_INTENSE_CYAN': '\001\033[0;106m\002',    # CYAN
     'BACKGROUND_INTENSE_WHITE': '\001\033[0;107m\002',   # WHITE
     }
+
+# These were taken directly from Python 3.5's source code
+class _RedirectStream:
+
+    _stream = None
+
+    def __init__(self, new_target):
+        self._new_target = new_target
+        # We use a list of old targets to make this CM re-entrant
+        self._old_targets = []
+
+    def __enter__(self):
+        self._old_targets.append(getattr(sys, self._stream))
+        setattr(sys, self._stream, self._new_target)
+        return self._new_target
+
+    def __exit__(self, exctype, excinst, exctb):
+        setattr(sys, self._stream, self._old_targets.pop())
+
+
+class redirect_stdout(_RedirectStream):
+    """Context manager for temporarily redirecting stdout to another file.
+
+        # How to send help() to stderr
+        with redirect_stdout(sys.stderr):
+            help(dir)
+
+        # How to write help() to a file
+        with open('help.txt', 'w') as f:
+            with redirect_stdout(f):
+                help(pow)
+    """
+
+    _stream = "stdout"
+
+
+class redirect_stderr(_RedirectStream):
+    """Context manager for temporarily redirecting stderr to another file."""
+
+    _stream = "stderr"
