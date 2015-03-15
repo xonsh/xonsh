0f5e4b2d254525301215de210e23c9f505e8bbb8
adam j hartz <hartz@mit.edu>
2015-03-14 22:51:26 -0400

    make use of argparse

--- xonsh/main.py
+++ xonsh/main.py
@@ -7,22 +7,24 @@ from argparse import ArgumentParser, Namespace
 
 from xonsh.shell import Shell
 
+parser = ArgumentParser(description='xonsh')
+parser.add_argument('-c', 
+        help="Run a single command and exit", 
+        dest='command', 
+        required=False, 
+        default=None)
+
 def main(argv=None):
     """Main entry point for xonsh cli."""
-    if argv is None:
-        argv = sys.argv[1:]
 
-    mode = 'shell'
-
-    if '-c' in argv:
-        mode = 'command'
-        argv.remove('-c')
+    args = parser.parse_args()
 
     shell = Shell()
-    if mode == 'shell':
+
+    if args.command is None:
         shell.cmdloop()
     else:
-        shell.default(' '.join(argv))
+        shell.default(args.command)
 
 if __name__ == '__main__':
     main()
