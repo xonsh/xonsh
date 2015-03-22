3155f24287583a70573581b031882e2588dcb5a6
adam j hartz <hartz@mit.edu>
2015-03-22 13:53:52 -0400

    updated syntax for arguments in xonsh scripts

--- xonsh/built_ins.py
+++ xonsh/built_ins.py
@@ -90,6 +90,14 @@ class Env(MutableMapping):
     #
 
     def __getitem__(self, key):
+        m = re.match(r'ARG(\d+)',key)
+        if m is not None and 'ARGS' in self._d.keys():
+            args = self._d['ARGS']
+            ix = int(m.group(1))
+            if ix >= len(args):
+                e = "Not enough arguments given to access argument {0}."
+                raise IndexError(e.format(ix))
+            return self._d['ARGS'][ix]
         return self._d[key]
 
     def __setitem__(self, key, val):
--- xonsh/main.py
+++ xonsh/main.py
@@ -47,9 +47,6 @@ def main(argv=None):
                 code = f.read()
             code = code if code.endswith('\n') else code + '\n'
             env['ARGS'] = [args.file] + args.args
-            env['0'] = args.file
-            for ix, arg in enumerate(args.args):
-                env[str(ix+1)] = arg
             code = shell.execer.compile(code, mode='exec', glbs=shell.ctx)
             shell.execer.exec(code, mode='exec', glbs=shell.ctx)
         else:
