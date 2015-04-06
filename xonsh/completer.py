4ae83a1034328d4cfa4e49a92b5dc1b6d2b38325
adam j hartz <hartz@mit.edu>
2015-04-06 08:26:20 -0400

    find highest mtime on first time through loop

--- xonsh/completer.py
+++ xonsh/completer.py
@@ -271,13 +271,18 @@ class Completer(object):
         al_hash = hash(tuple(builtins.aliases.keys()))
         self._alias_checksum = al_hash
         cache_valid = cache_valid and al_hash == self._alias_checksum
+        pm = self._path_mtime
         if cache_valid:
             for d in filter(os.path.isdir, path):
                 m = os.stat(d).st_mtime
-                if m > self._path_mtime:
-                    self._path_mtime = m
+                if m > pm:
+                    pm = m
                     cache_valid = False
-                    break
+                    # run through everything the first time to get the highest
+                    # mtime for the cache, to avoid rebuilding multiple times
+                    if self._path_mtime != -1:
+                        break
+        self._path_mtime = pm
         if cache_valid:
             return self._cmds_cache
         allcmds = set()
