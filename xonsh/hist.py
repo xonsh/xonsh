82fa3795514c984de2f28ed4bd995ab0d60e13ac
Aaron Griffin <aig787@gmail.com>
2015-04-23 09:22:13 -0700

    Cleaned up history module and added docstrings

--- xonsh/hist.py
+++ xonsh/hist.py
@@ -9,13 +9,34 @@ class History(object):
     ordered_history = OrderedDict()
 
     def load_history(self, hist_file="~/.xonsh_history.json"):
+        """Loads previous history from ~/.xonsh_history.json if it exists.
+
+        Parameters
+        ----------
+        hist_file : str, optional
+            Path of the history file.
+
+        Returns
+        -------
+            None
+        """
         try: 
             with open(hist_file) as data_file:
                 json_history = json.load(data_file)
-            print(str(json_history))
         except:
             pass
 
 
     def add(self, cmd):
+        """Adds command with current timestamp to ordered history.
+
+        Parameters
+        ----------
+        cmd: str
+            Command that should be added to the ordered history.
+
+        Returns
+        -------
+            None
+        """
         self.ordered_history[time.time()] = {'cmd': cmd.strip()}
