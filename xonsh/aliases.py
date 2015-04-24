4aa89556e198a2bbe5032f5b64ae3063360df315
Aaron Griffin <aig787@gmail.com>
2015-04-24 14:23:14 -0700

    Added number of arguments to history alias, implemented \!! and \!n aliases

--- xonsh/aliases.py
+++ xonsh/aliases.py
@@ -1,6 +1,7 @@
 """Aliases for the xonsh shell.
 """
 import os
+import sys
 import platform
 import builtins
 import subprocess
@@ -65,22 +66,60 @@ def xexec(args, stdin=None):
 
 def history(args, stdin=None):
     """
-    Prints last 10 commands executed  in the format timestamp: command.
+    Prints last n commands executed  in the format timestamp: command.
+
+    usage: history [n], where n is an optional number of commands to print.
     """
+    num = 10
+    if len(args) > 0:
+        try:
+            num = int(args[0])
+        except ValueError:
+            return 'xonsh: history: usage: history [-r] [number]'
     hist_str = ''
     reversed_history = reversed(builtins.ordered_history)
-    for i in range(10):
+    # Skip this command in history
+    next(reversed_history)
+    for i in range(num):
         try:
-            timestamp = next(reversed_history)
+            entry = next(reversed_history)
         except StopIteration:
             break 
-        entry = builtins.ordered_history[timestamp]
-        timestamp = datetime.datetime.fromtimestamp(int(timestamp)
+        timestamp = datetime.datetime.fromtimestamp(int(entry['timestamp'])
                     ).strftime('%Y-%m-%d %H:%M:%S') + ": " 
-        cmd = '\033[1m' + entry['cmd'].strip() + '\033[0m'
+        cmd = '\033[1m' + entry['cmd'].rstrip() + '\033[0m'
         cmd = cmd.replace('\n', '\n' + ' '*len(timestamp) + ' ') + '\n'
         hist_str += '{} {}'.format(timestamp, cmd)
     return hist_str
+
+
+def bang_bang(args, stdin=None):
+    """
+    Re-runs the last command. Just a wrapper around bang_n.
+    """
+    return bang_n(['1'])
+
+
+def bang_n(args, stdin=None):
+    """
+    Re-runs the nth command as specified in the argument.
+    """
+    if len(args) == 1:
+        try:
+            # 1 is subtracted here to exclude the current command
+            index = -int(args[0])-1
+        except:
+            return 'xonsh: !n: usage: !n n\n'
+        if len(builtins.ordered_history) >= abs(index):
+            cmd = builtins.ordered_history[index]['cmd']
+            if '!!' not in cmd and '!n' not in cmd:
+                builtins.execx(builtins.ordered_history[index]['cmd'])
+            else:
+                return 'xonsh: error: recursive call to !! or !n\n'
+        else:
+            return 'xonsh: no previous command\n'
+    else:
+        return 'xonsh: !n: usage: !n n\n'
 
 
 def bash_aliases():
@@ -120,6 +159,8 @@ DEFAULT_ALIASES = {
     'quit': exit,
     'xexec': xexec,
     'history': history,
+    '!!': bang_bang,
+    '!n': bang_n,
     'source-bash': source_bash,
     'grep': ['grep', '--color=auto'],
     'scp-resume': ['rsync', '--partial', '-h', '--progress', '--rsh=ssh'],
