889d8eb68e65e714233b6a018240594d3014f981
Anthony Scopatz <scopatz@gmail.com>
2015-05-07 19:43:20 -0500

    something that might work

--- xonsh/timings.py
+++ xonsh/timings.py
@@ -1,16 +1,100 @@
-"""Timing related functionality for the xonsh shell."""
+"""Timing related functionality for the xonsh shell.
+
+The following time_it alias and Timer was forked from the IPython project:
+* Copyright (c) 2008-2014, IPython Development Team
+* Copyright (C) 2001-2007 Fernando Perez <fperez@colorado.edu>
+* Copyright (c) 2001, Janko Hauser <jhauser@zscout.de>
+* Copyright (c) 2001, Nathaniel Gray <n8gray@caltech.edu>
+"""
+from __future__ import unicode_literals, print_function
 import gc
+import math
 import time
 import timeit
 import builtins
 import itertools
 
+try:
+    import resource
+    _HAVE_RESOURCE = True
+except ImportError:
+    # There is no distinction of user/system time under windows, so we 
+    # just use time.clock() for everything...
+    resource = None
+    _HAVE_RESOURCE = False
 
-# The following time_it alias and Timer was forked from the IPython project:
-# * Copyright (c) 2008-2014, IPython Development Team
-# * Copyright (C) 2001-2007 Fernando Perez <fperez@colorado.edu>
-# * Copyright (c) 2001, Janko Hauser <jhauser@zscout.de>
-# * Copyright (c) 2001, Nathaniel Gray <n8gray@caltech.edu>
+if _HAVE_RESOURCE:
+    def clocku():
+        """clocku() -> floating point number
+        Return the *USER* CPU time in seconds since the start of the process.
+        This is done via a call to resource.getrusage, so it avoids the
+        wraparound problems in time.clock()."""
+        return resource.getrusage(resource.RUSAGE_SELF)[0]
+
+    def clocks():
+        """clocks() -> floating point number
+        Return the *SYSTEM* CPU time in seconds since the start of the process.
+        This is done via a call to resource.getrusage, so it avoids the
+        wraparound problems in time.clock()."""
+        return resource.getrusage(resource.RUSAGE_SELF)[1]
+
+    def clock():
+        """clock() -> floating point number
+        Return the *TOTAL USER+SYSTEM* CPU time in seconds since the start of
+        the process.  This is done via a call to resource.getrusage, so it
+        avoids the wraparound problems in time.clock()."""
+        u, s = resource.getrusage(resource.RUSAGE_SELF)[:2]
+        return u + s
+
+    def clock2():
+        """clock2() -> (t_user,t_system)
+        Similar to clock(), but return a tuple of user/system times."""
+        return resource.getrusage(resource.RUSAGE_SELF)[:2]
+else:
+    clocku = clocks = clock = time.clock
+    def clock2():
+        """Under windows, system CPU time can't be measured.
+        This just returns clock() and zero."""
+        return time.clock(), 0.0
+
+
+def format_time(timespan, precision=3):
+    """Formats the timespan in a human readable form"""
+    if timespan >= 60.0:
+        # we have more than a minute, format that in a human readable form
+        parts = [("d", 60*60*24),("h", 60*60),("min", 60), ("s", 1)]
+        time = []
+        leftover = timespan
+        for suffix, length in parts:
+            value = int(leftover / length)
+            if value > 0:
+                leftover = leftover % length
+                time.append('{0}{1}'.format(str(value), suffix))
+            if leftover < 1:
+                break
+        return " ".join(time)
+    # Unfortunately the unicode 'micro' symbol can cause problems in
+    # certain terminals.  
+    # See bug: https://bugs.launchpad.net/ipython/+bug/348466
+    # Try to prevent crashes by being more secure than it needs to
+    # E.g. eclipse is able to print a µ, but has no sys.stdout.encoding set.
+    units = ["s", "ms", 'us', "ns"]  # the save value   
+    if hasattr(sys.stdout, 'encoding') and sys.stdout.encoding:
+        try:
+            '\xb5'.encode(sys.stdout.encoding)
+            units = ["s", "ms", '\xb5s', "ns"]
+        except:
+            pass
+    scaling = [1, 1e3, 1e6, 1e9]
+        
+    if timespan > 0.0:
+        order = min(-int(math.floor(math.log10(timespan)) // 3), 3)
+    else:
+        order = 3
+    return "{1:.{0}g} {2}".format(precision, timespan * scaling[order],
+                                  units[order])
+
+
 class Timer(timeit.Timer):
     """Timer class that explicitly uses self.inner
     which is an undocumented implementation detail of CPython,
@@ -47,11 +131,57 @@ def inner(_it, _timer):
     return _t1 - _t0
 """
 
-def time_it(args, stdin=None):
+def timeit_alias(args, stdin=None):
     """Runs timing study on arguments."""
-    timer = Timer(timer=timefunc)
+    # some real args
+    number = 0
+    quiet = False
+    # setup
+    ctx = builtins.__xonsh_ctx__
+    timer = Timer(timer=clock)
     stmt = ' '.join(args)
     innerstr = INNER_TEMPLATE.format(stmt=stmt)
-    inner = builtins.compilex(innerstr, )
+    # Track compilation time so it can be reported if too long
+    # Minimum time above which compilation time will be reported
+    tc_min = 0.1
+    t0 = clock()
+    innercode = builtins.compilex(innerstr, '<xonsh-timeit>', mode='exec',
+                                  glbs=ctx)
+    tc = clock() - t0
+    # get inner func
+    ns = {}
+    builtins.execx(code, glbs=ctx, locs=ns, mode='exec')
+    timer.inner = ns['inner']
+    # Check if there is a huge difference between the best and worst timings.
+    worst_tuning = 0
+    if number == 0:
+        # determine number so that 0.2 <= total time < 2.0
+        number = 1
+        for _ in range(1, 10):
+            time_number = timer.timeit(number)
+            worst_tuning = max(worst_tuning, time_number / number)
+            if time_number >= 0.2:
+                    break
+            number *= 10
+    all_runs = timer.repeat(repeat, number)
+    best = min(all_runs) / number
+    # print some debug info
+    if not quiet:
+        worst = max(all_runs) / number
+        if worst_tuning:
+            worst = max(worst, worst_tuning)
+        # Check best timing is greater than zero to avoid a
+        # ZeroDivisionError.
+        # In cases where the slowest timing is lesser than a micosecond
+        # we assume that it does not really matter if the fastest
+        # timing is 4 times faster than the slowest timing or not.
+        if worst > 4 * best and best > 0 and worst > 1e-6:
+            print(('The slowest run took {0:0.2f} times longer than the '
+                   'fastest. This could mean that an intermediate result '
+                   'is being cached.').format(worst / best))
+        print("{0} loops, best of {1}: {2} per loop"
+              .format(number, repeat, format_time(best, precision)))
+        if tc > tc_min:
+            print("Compiler time: {0:.2f} s".format(t))
     return
 
