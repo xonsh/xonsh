576f1c5916ad3cbfd198125b2725513069da625f
adam j hartz <hartz@mit.edu>
2015-05-12 13:35:13 -0400

    additional imports from subprocess

--- xonsh/proc.py
+++ xonsh/proc.py
@@ -4,7 +4,7 @@ import sys
 import platform
 
 from threading import Thread
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL, STDOUT
 from collections import Sequence
 
 from xonsh.tools import redirect_stdout, redirect_stderr
