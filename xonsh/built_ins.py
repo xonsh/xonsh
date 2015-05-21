faf861cefdd351172d9fd51b9becfb2b8742e2f0
adam j hartz <hartz@mit.edu>
2015-05-21 05:42:01 -0400

    io bugfix

--- xonsh/built_ins.py
+++ xonsh/built_ins.py
@@ -300,8 +300,9 @@ _WRITE_MODES = frozenset({'w', 'a'})
 _REDIR_ALL = frozenset({'&', 'a', 'all'})
 _REDIR_ERR = frozenset({'2', 'e', 'err'})
 _REDIR_OUT = frozenset({'', '1', 'o', 'out'})
-_E2O_MAP = frozenset({'e>o', 'e>out', 'err>o', 'err>o', '2>1',
-                              'e>1', 'err>1', '2>out', '2>o'})
+_E2O_MAP = frozenset({'e>o', 'e>out', 'err>o', 'err>out', '2>1',
+                      'e>1', 'err>1', '2>out', '2>o'})
+
 
 def _is_redirect(x):
     return isinstance(x, str) and _REDIR_REGEX.match(x)
