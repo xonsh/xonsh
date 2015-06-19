05d1167182abf1282c2f6199e0836e1eb1e0599b
Anthony Scopatz <scopatz@gmail.com>
2015-06-19 14:40:11 +0300

    improved man tests

--- tests/test_man.py
+++ tests/test_man.py
@@ -1,19 +1,35 @@
 import os
-import platform
 
 import nose
 from nose.tools import assert_true
+from nose.plugins.skip import SkipTest
 
+from xonsh.tools import ON_WINDOWS
 from xonsh.completer import ManCompleter
 
-os.environ['MANPATH'] = os.path.dirname(os.path.abspath(__file__))
+_OLD_MANPATH = None
+
+def setup():
+    global _OLD_MANPATH
+    _OLD_MANPATH = os.environ.get('MANPATH', None)
+    os.environ['MANPATH'] = os.path.dirname(os.path.abspath(__file__))
+
+
+def teardown():
+    global _OLD_MANPATH
+    if _OLD_MANPATH is None:
+        del os.environ['MANPATH']
+    else:
+        os.environ['MANPATH'] = _OLD_MANPATH
+
 
 def test_man_completion():
-    if (platform.system() != 'Windows'):
-        man_completer = ManCompleter()
-        completions = man_completer.option_complete('--', 'yes')
-        assert_true('--version' in completions)
-        assert_true('--help' in completions)
+    if ON_WINDOWS:
+        raise SkipTest
+    man_completer = ManCompleter()
+    completions = man_completer.option_complete('--', 'yes')
+    assert_true('--version' in completions)
+    assert_true('--help' in completions)
 
 
 if __name__ == '__main__':
