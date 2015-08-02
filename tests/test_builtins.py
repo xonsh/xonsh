37c2c26dd6e0b5f6d26ea94e5f0df3b4d79762d0
Anthony Scopatz <scopatz@gmail.com>
2015-08-02 13:25:49 -0500

    more win tests 3

--- tests/test_builtins.py
+++ tests/test_builtins.py
@@ -26,9 +26,10 @@ def test_repath_backslash():
     exp = os.listdir(home)
     exp = {p for p in exp if re.match(r'\w\w.*', p)}
     exp = {os.path.join(home, p) for p in exp}
-    obs = set(regexpath(r'~\\\\\w\w.*'))
+    obs = set(regexpath(r'~/\w\w.*'))
     print("expected\n", exp)
     print("\n=======\nobs\n", obs)
+    print("On Windows: ", ON_WINDOWS)
     assert_equal(exp, obs)
 
 def test_repath_home_itself():
