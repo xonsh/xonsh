d6381e18977b435cfc75be0bd7d7f76023a21989
Anthony Scopatz <scopatz@gmail.com>
2015-08-11 23:05:14 -0400

    some test updates

--- tests/test_history.py
+++ tests/test_history.py
@@ -3,39 +3,38 @@ from __future__ import unicode_literals, print_function
 import os
 
 import nose
-from nose.tools import assert_equal, assert_true, assert_not_in
+from nose.tools import assert_equal, assert_true
 
 from xonsh.history import History
 from xonsh.lazyjson import LazyJSON
 
-FNAME = 'xonsh-SESSIONID.json'
-HIST_TEST_KWARGS = dict(filename=FNAME, sessionid='SESSIONID', gc=False)
-
-
-def teardown():
-    if os.path.isfile(FNAME):
-        os.remove(FNAME)
+HIST_TEST_KWARGS = dict(sessionid='SESSIONID', gc=False)
 
 
 def test_hist_init():
-    hist = History(here='yup', **HIST_TEST_KWARGS)
+    FNAME = 'xonsh-SESSIONID.json'
+    FNAME += '.init'
+    hist = History(filename=FNAME, here='yup', **HIST_TEST_KWARGS)
     with LazyJSON(FNAME, reopen=False) as lj:
         obs = lj['here']
     assert_equal('yup', obs)
-    with open(FNAME) as f:
-        s = f.read()
-    print(s)
+    os.remove(FNAME)
 
 
 def test_hist_append():
-    hist = History(here='yup', **HIST_TEST_KWARGS)
+    FNAME = 'xonsh-SESSIONID.json'
+    FNAME += '.append'
+    hist = History(filename=FNAME, here='yup', **HIST_TEST_KWARGS)
     hf = hist.append({'joco': 'still alive'})
     yield assert_true, hf is None
     yield assert_equal, 'still alive', hist.buffer[0]['joco']
+    os.remove(FNAME)
 
 
 def test_hist_flush():
-    hist = History(here='yup', **HIST_TEST_KWARGS)
+    FNAME = 'xonsh-SESSIONID.json'
+    FNAME += '.flush'
+    hist = History(filename=FNAME, here='yup', **HIST_TEST_KWARGS)
     hf = hist.flush()
     yield assert_true, hf is None
     hist.append({'joco': 'still alive'})
@@ -46,6 +45,7 @@ def test_hist_flush():
     with LazyJSON(FNAME, reopen=False) as lj:
         obs = lj['cmds'][0]['joco']
     yield assert_equal, 'still alive', obs
+    os.remove(FNAME)
 
 
 
