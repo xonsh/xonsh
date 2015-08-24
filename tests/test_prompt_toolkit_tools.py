ca6a1753c9f8b81bacc68132a9299e9dd4ef3672
Morten Enemark Lund <melund@gmail.com>
2015-08-24 09:48:24 +0200

    Update test for prompt_toolkit_tools

--- tests/test_prompt_toolkit_tools.py
+++ tests/test_prompt_toolkit_tools.py
@@ -8,14 +8,16 @@ from xonsh.tools import format_prompt_for_prompt_toolkit
 from xonsh.tools import TERM_COLORS
 from xonsh.environ import format_prompt
 
+
+
 def test_format_prompt_for_prompt_toolkit():
-    
     templ = ('>>> {BOLD_BLUE}~/xonsh {WHITE} (main){NO_COLOR}')
     prompt = format_prompt(templ, TERM_COLORS)
     token_names, color_styles, strings = format_prompt_for_prompt_toolkit(prompt)
-    assert_equal(token_names, ['No_Color', 'BOLD_BLUE', 'WHITE', 'NO_COLOR'] )
-    assert_equal(color_styles, ['', 'bold #0000FF', '#FFFFFF', ''] )
+    assert_equal(token_names, ['NO_COLOR', 'BOLD_BLUE', 'WHITE', 'NO_COLOR'])
+    assert_equal(color_styles, ['', 'bold #0000FF', '#FFFFFF', ''])
     assert_equal(strings, ['>>> ', '~/xonsh ', ' (main)', ''])
+
 
 
 if __name__ == '__main__':
