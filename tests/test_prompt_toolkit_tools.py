1b20b53785ce6c60ea47638afccd5e0acc94c74c
adam j hartz <hartz@mit.edu>
2015-11-29 10:57:30 -0500

    add custom color case to tests

--- tests/test_prompt_toolkit_tools.py
+++ tests/test_prompt_toolkit_tools.py
@@ -11,14 +11,14 @@ from xonsh.tools import TERM_COLORS
 from xonsh.environ import format_prompt, Env
 
 builtins.__xonsh_env__ = Env()
-
+builtins.__xonsh_env__['PROMPT_TOOLKIT_COLORS'] = {'WHITE': '#ffffff'}
 
 def test_format_prompt_for_prompt_toolkit():
     templ = ('>>> {BOLD_BLUE}~/xonsh {WHITE} (main){NO_COLOR}')
     prompt = format_prompt(templ, TERM_COLORS)
     token_names, color_styles, strings = format_prompt_for_prompt_toolkit(prompt)
     assert_equal(token_names, ['NO_COLOR', 'BOLD_BLUE', 'WHITE', 'NO_COLOR'])
-    assert_equal(color_styles, ['', 'bold #0000FF', '#FFFFFF', ''])
+    assert_equal(color_styles, ['', 'bold #0000FF', '#ffffff', ''])
     assert_equal(strings, ['>>> ', '~/xonsh ', ' (main)', ''])
 
 
