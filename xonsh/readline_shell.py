d2948670511b165c670ab814d14464fb45b8bb25
adam j hartz <hartz@mit.edu>
2015-12-18 12:09:21 -0500

    workaround for readline completion deleting too many things

--- xonsh/readline_shell.py
+++ xonsh/readline_shell.py
@@ -110,9 +110,10 @@ class ReadlineShell(BaseShell, Cmd):
         rl_completion_suppress_append()  # this needs to be called each time
         mline = line.partition(' ')[2]
         offs = len(mline) - len(text)
-        x = [i[offs:] for i in self.completer.complete(text, line,
-                                                       begidx, endidx,
-                                                       ctx=self.ctx)[0]]
+        x = [(i[offs:] if " " in i[:-1] else i)
+             for i in self.completer.complete(text, line,
+                                              begidx, endidx,
+                                              ctx=self.ctx)[0]]
         return x
 
     # tab complete on first index too
