6bf3320ff191542251ad275b886d027b1e47e7ac
Gil Forsyth <gilforsyth@gmail.com>
2016-01-11 17:09:20 -0500

    add tests for most multiline prompt behavior

--- tests/test_ptk_multiline.py
+++ tests/test_ptk_multiline.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals, print_function
 
 import nose
 from nose.tools import assert_equal
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import builtins
 from xonsh.ptk.key_bindings import carriage_return
@@ -16,18 +16,51 @@ from prompt_toolkit.buffer import Buffer, AcceptAction
 
 builtins.__xonsh_env__ = Env()
 builtins.__xonsh_env__['INDENT'] = '    '
-env = builtins.__xonsh_env__
+indent_ = '    '
 ##setup
-document = Document('for i in range(5):')
-buffer = Buffer(initial_document=document)
+buffer = Buffer()
 bufaccept = MagicMock(name='accept', spec=AcceptAction)
 cli = MagicMock(name='cli', spec=CommandLineInterface)
 
+
 buffer.accept_action = bufaccept
 
-def test_newline_indent():
-    carriage_return(buffer, cli, env.get('INDENT'))
-    assert_equal(buffer.document.current_line, env.get('INDENT'))
+def test_colon_indent():
+    document = Document('for i in range(5):')
+    buffer.set_document(document)
+    carriage_return(buffer, cli, indent_)
+    assert_equal(buffer.document.current_line, indent_)
+
+def test_dedent():
+    document = Document(indent_+'pass')
+    buffer.set_document(document)
+    carriage_return(buffer, cli, indent_)
+    assert_equal(buffer.document.current_line, '')
+
+    document = Document(2*indent_+'continue')
+    buffer.set_document(document)
+    carriage_return(buffer, cli, indent_)
+    assert_equal(buffer.document.current_line,indent_)
+
+def test_continuation_line():
+    document = Document('\nsecond line')
+    buffer.set_document(document)
+    carriage_return(buffer, cli, '')
+    assert_equal(buffer.document.current_line, '')
+
+def test_trailing_slash():
+    document = Document('this line will \\')
+    buffer.set_document(document)
+    carriage_return(buffer, cli, '')
+    assert_equal(buffer.document.current_line, '')
+
+def test_cant_compile_newline():
+    mock = MagicMock(return_value = False)
+    with patch('xonsh.ptk.key_bindings.can_compile', mock):
+        document = Document('for i in (1, 2, ')
+        buffer.set_document(document)
+        carriage_return(buffer, cli, '')
+        assert_equal(buffer.document.current_line, '')
 
 if __name__ == '__main__':
     nose.runmodule()
