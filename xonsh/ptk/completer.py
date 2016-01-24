be6bc4522c2f79fb09ebeb71971750644c1c70bc
Gil Forsyth <gforsyth@gwu.edu>
2016-01-24 12:23:18 -0500

    add temporary fix for diverging prompt_toolkit layout

--- xonsh/ptk/completer.py
+++ xonsh/ptk/completer.py
@@ -41,7 +41,11 @@ class PromptToolkitCompleter(Completer):
 
     def reserve_space(self):
         cli = builtins.__xonsh_shell__.shell.prompter.cli
-        window = cli.application.layout.children[1].children[1].content
+        #TODO remove after next prompt_toolkit release
+        try:
+            window = cli.application.layout.children[1].children[1].content
+        except AttributeError:
+            window = cli.application.layout.children[1].content
         h = window.render_info.content_height
         r = builtins.__xonsh_env__.get('COMPLETIONS_MENU_ROWS')
         size = h + r
