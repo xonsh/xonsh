db4d9787dec47611c76aecd09bb6eb966c9f7334
adam j hartz <hartz@mit.edu>
2016-03-20 23:38:16 -0400

    small refactor

--- xonsh/xoreutils/__init__.py
+++ xonsh/xoreutils/__init__.py
@@ -2,5 +2,3 @@ from xonsh.xoreutils.echo import echo
 from xonsh.xoreutils.pwd import pwd
 from xonsh.xoreutils.cat import cat
 from xonsh.xoreutils.tty import tty
-
-__all__ = ['echo', 'pwd', 'cat', 'tty']
