8eca913d845fd8e4c2d6ea46e792ea93f4cf5ed7
adam j hartz <hartz@mit.edu>
2016-03-21 00:00:06 -0400

    update pwd command

--- xonsh/xoreutils/pwd.py
+++ xonsh/xoreutils/pwd.py
@@ -1,2 +1,18 @@
+import os
+
 def pwd(args, stdin, stdout, stderr):
-    print(__xonsh_env__['PWD'], file=stdout)
+    e = __xonsh_env__['PWD']
+    if '-P' in args:
+        e = os.path.realpath(e)
+    print(e, file=stdout)
+
+
+HELP_STR = """This version of pwd was written in Python for the xonsh project: http://xon.sh
+Based on pwd from GNU coreutils: http://www.gnu.org/software/coreutils/
+
+Usage: pwd [OPTION]...
+Print the full filename of the current working directory.
+
+  -L, --logical    use PWD from environment, even if it contains symlinks
+  -P, --physical   avoid all symlinks
+      --help       display this help and exit"""
