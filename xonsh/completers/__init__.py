f5299fd2834682bc5f554e4e7c9a1bb71fbeae10
adam j hartz <hartz@mit.edu>
2016-05-29 11:43:09 -0400

    add bash completer

--- xonsh/completers/__init__.py
+++ xonsh/completers/__init__.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 
 from xonsh.completers.man import complete_from_man
+from xonsh.completers.bash import complete_from_bash
 from xonsh.completers.base import complete_base
 from xonsh.completers.path import complete_path
 from xonsh.completers.dirs import complete_cd, complete_rmdir
@@ -9,6 +10,7 @@ from xonsh.completers.commands import complete_skipper
 
 completers = OrderedDict()
 completers['base'] = complete_base
+completers['bash'] = complete_from_bash
 completers['man'] = complete_from_man
 completers['skip'] = complete_skipper
 completers['cd'] = complete_cd
