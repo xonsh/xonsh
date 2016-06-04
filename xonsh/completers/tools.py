079e07d27b43636940029d75e62abb4af8f820a4
adam j hartz <hartz@mit.edu>
2016-06-04 16:47:01 -0400

    some more changes, per @scopatz's suggestions

--- xonsh/completers/tools.py
+++ xonsh/completers/tools.py
@@ -1,16 +1,32 @@
 import builtins
 
 
+def _filter_normal(s, x):
+    return s.startswith(x)
+
+
+def _filter_ignorecase(s, x):
+    return s.lower().startswith(x.lower())
+
+
 def get_filter_function():
+    """
+    Return an appropriate filtering function for completions, given the valid
+    of $CASE_SENSITIVE_COMPLETIONS
+    """
     csc = builtins.__xonsh_env__.get('CASE_SENSITIVE_COMPLETIONS')
     if csc:
-        def filt(s, x): return s.startswith(x)
+        return _filter_normal
     else:
-        def filt(s, x): return s.lower().startswith(x.lower())
-    return filt
+        return _filter_ignorecase
 
 
 def justify(s, max_length, left_pad=0):
+    """
+    Re-wrap the string s so that each line is no more than max_length
+    characters long, padding all lines but the first on the left with the
+    string left_pad.
+    """
     toks = s.strip().split()
     lines = [[]]
     for tok in toks:
