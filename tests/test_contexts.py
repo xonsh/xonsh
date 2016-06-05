6e387e521bf14f42d63e218e67e0517db8495a7b
Anthony Scopatz <scopatz@gmail.com>
2016-06-05 18:25:52 -0400

    functional tests

--- tests/test_contexts.py
+++ tests/test_contexts.py
@@ -1,5 +1,5 @@
 """Tests xonsh contexts."""
-from nose.tools import assert_equal, assert_is
+from nose.tools import assert_equal, assert_is, assert_is_not
 
 from tools import (mock_xonsh_env, execer_setup, check_exec, check_eval,
     check_parse, skip_if)
@@ -13,6 +13,24 @@ from xonsh.contexts import Block
 def setup():
     execer_setup()
 
+
+X1_WITH = ('x = 1\n'
+           'with Block() as b:\n')
+SIMPLE_WITH = 'with Block() as b:\n'
+FUNC_WITH = ('x = 1\n'
+             'def func():\n'
+             '    y = 1\n'
+             '    with Block() as b:\n'
+             '{body}'
+             '    y += 1\n'
+             '    return b\n'
+             'x += 1\n'
+             'rtn = func()\n'
+             'x += 1\n')
+
+FUNC_OBSG = {'x': 3}
+FUNC_OBSL = {'y': 1}
+
 def block_checks_glb(name, glbs, body, obs=None):
     block = glbs[name]
     obs = obs or {}
@@ -24,9 +42,22 @@ def block_checks_glb(name, glbs, body, obs=None):
     yield assert_is, glbs, block.glbs
     yield assert_is, None, block.locs
 
-X1_WITH = ('x = 1\n'
-           'with Block() as b:\n')
-SIMPLE_WITH = 'with Block() as b:\n'
+
+def block_checks_func(name, glbs, body, obsg=None, obsl=None):
+    block = glbs[name]
+    obsg = obsg or {}
+    for k, v in obsg.items():
+        yield assert_equal, v, glbs[k]
+    if isinstance(body, str):
+        body = body.splitlines()
+    yield assert_equal, body, block.lines
+    yield assert_is, glbs, block.glbs
+    # local context tests
+    locs = block.locs
+    yield assert_is_not, None, locs
+    obsl = obsl or {}
+    for k, v in obsl.items():
+        yield assert_equal, v, locs[k]
 
 
 #
@@ -107,4 +138,25 @@ def test_block_trailing_close_many():
     glbs = {'Block': Block}
     check_exec(s, glbs=glbs, locs=None)
     yield from block_checks_glb('b', glbs, body)
+
+
+def test_block_trailing_triple_string():
+    body = ('    x = """This\n'
+            'is\n'
+            '"probably"\n'
+            '\'not\' what I meant.\n'
+            '"""\n')
+    s = SIMPLE_WITH + body
+    glbs = {'Block': Block}
+    check_exec(s, glbs=glbs, locs=None)
+    yield from block_checks_glb('b', glbs, body)
+
+
+def test_block_func_oneline():
+    body = '        x += 42\n'
+    s = FUNC_WITH.format(body=body)
+    glbs = {'Block': Block}
+    check_exec(s, glbs=glbs, locs=None)
+    yield from block_checks_func('rtn', glbs, body, FUNC_OBSG, FUNC_OBSL)
+
 
