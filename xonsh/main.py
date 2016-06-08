7114df3b3afd09f74fe269fae3608836ad67da3c
cryzed <cryzed@googlemail.com>
2016-06-09 00:27:07 +0200

    Implement #1169

--- xonsh/main.py
+++ xonsh/main.py
@@ -240,9 +240,11 @@ def main(argv=None):
         run_code_with_cache(args.command, shell.execer, mode='single')
     elif args.mode == XonshMode.script_from_file:
         # run a script contained in a file
-        if os.path.isfile(args.file):
+        path = os.path.abspath(os.path.expanduser(args.file))
+        if os.path.isfile(path):
             sys.argv = args.args
             env['ARGS'] = [args.file] + args.args
+            env['XONSH_SOURCE'] = path
             run_script_with_cache(args.file, shell.execer, glb=shell.ctx, loc=None, mode='exec')
         else:
             print('xonsh: {0}: No such file or directory.'.format(args.file))
