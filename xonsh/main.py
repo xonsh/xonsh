d237ab748df7edf7d4468d459a6cf1c163f67368
cryzed <cryzed@googlemail.com>
2016-06-09 00:18:09 +0200

    Remove duplicate "-c" key in arg_undoers

--- xonsh/main.py
+++ xonsh/main.py
@@ -120,7 +120,6 @@ def arg_undoers():
         '-c': (lambda args: setattr(args, 'command', None)),
         '-i': (lambda args: setattr(args, 'force_interactive', False)),
         '-l': (lambda args: setattr(args, 'login', False)),
-        '-c': (lambda args: setattr(args, 'command', None)),
         '--no-script-cache': (lambda args: setattr(args, 'scriptcache', True)),
         '--cache-everything': (lambda args: setattr(args, 'cacheall', False)),
         '--config-path': (lambda args: delattr(args, 'config_path')),
