4400a6f98a6a12d1c7a7e97b29e6f1cfcbfc905c
Jeremy Donahue <jeremydonahue@gmail.com>
2016-06-08 18:43:39 -0700

    Implement '+' and '-' for fg
    Implement '+' and '-' for fg for BASHwards compatability.

--- xonsh/jobs.py
+++ xonsh/jobs.py
@@ -240,7 +240,8 @@ def fg(args, stdin=None):
     xonsh command: fg
 
     Bring the currently active job to the foreground, or, if a single number is
-    given as an argument, bring that job to the foreground.
+    given as an argument, bring that job to the foreground. Additionally,
+    specify "+" for the most recent job and "-" for the second most recent job.
     """
 
     _clear_dead_jobs()
@@ -250,10 +251,15 @@ def fg(args, stdin=None):
     if len(args) == 0:
         act = tasks[0]  # take the last manipulated task by default
     elif len(args) == 1:
-        try:
-            act = int(args[0])
-        except ValueError:
-            return '', 'Invalid job: {}\n'.format(args[0])
+        if args[0] == '+': # take the last manipulated task
+             act = tasks[0]
+        elif args[0] == '-': # take the second to last manipulated task
+            act = tasks[1]
+        else:
+            try:
+                act = int(args[0])
+            except ValueError:
+                return '', 'Invalid job: {}\n'.format(args[0])
         if act not in builtins.__xonsh_all_jobs__:
             return '', 'Invalid job: {}\n'.format(args[0])
     else:
