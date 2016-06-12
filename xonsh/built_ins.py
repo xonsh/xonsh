b7baa64abdf4c09c4cbc1c02a57f841ee5f588b5
adam j hartz <hartz@mit.edu>
2016-06-11 21:35:58 -0400

    fixes for search funcs

--- xonsh/built_ins.py
+++ xonsh/built_ins.py
@@ -148,23 +148,23 @@ def pathsearch(s, pymode=False):
     """
     searchfunc, pattern = re.match(SearchPath, s).groups()
     if searchfunc == 'r' or searchfunc == '':
-        o = regexsearch(s)
+        o = regexsearch(pattern)
     elif searchfunc == 'g':
-        o = globsearch(s)
+        o = globsearch(pattern)
     else:
         ctx = builtins.__xonsh_ctx__
         searchfunc = searchfunc[1:]
         if (searchfunc not in ctx or
                 not callable(ctx[searchfunc]) or
-                len(inspect.signature(aliased_cmd).parameters) != 1):
+                len(inspect.signature(ctx[searchfunc]).parameters) != 1):
             error = "%r is not a known path search function"
             raise XonshError(error % searchfunc)
         try:
             o = ctx[searchfunc](pattern)
         except Exception:
             o = []
-        no_match = [] if pymode else [pattern]
-        return o if len(o) != 0 else no_match
+    no_match = [] if pymode else [pattern]
+    return o if len(o) != 0 else no_match
 
 RE_SHEBANG = re.compile(r'#![ \t]*(.+?)$')
 
@@ -686,7 +686,6 @@ def load_builtins(execer=None, config=None, login=False, ctx=None):
     builtins.__xonsh_help__ = helper
     builtins.__xonsh_superhelp__ = superhelper
     builtins.__xonsh_pathsearch__ = pathsearch
-    builtins.__xonsh_path_search_functions__ = PATH_SEARCHERS
     builtins.__xonsh_glob__ = globpath
     builtins.__xonsh_expand_path__ = expand_path
     builtins.__xonsh_exit__ = False
@@ -717,8 +716,6 @@ def load_builtins(execer=None, config=None, login=False, ctx=None):
     builtins.compilex = None if execer is None else execer.compile
 
     # sneak the path search functions into the aliases
-    builtins.__xonsh_env__['PATH_SEARCH_FUNCS'] = PATH_SEARCHERS
-
     # Need this inline/lazy import here since we use locate_binary that relies on __xonsh_env__ in default aliases
     builtins.default_aliases = builtins.aliases = Aliases(make_default_aliases())
     if login:
@@ -757,7 +754,6 @@ def unload_builtins():
              '__xonsh_help__',
              '__xonsh_superhelp__',
              '__xonsh_pathsearch__',
-             '__xonsh_path_search_functions__',
              '__xonsh_glob__',
              '__xonsh_expand_path__',
              '__xonsh_exit__',
