aa163fbc0a1bca1dcfca8abb441d1f36d3c9b49d
Konstantinos Tsakiltzidis <laerusk@gmail.com>
2016-06-22 23:20:37 +0300

    'replay/wizard'

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -5,8 +5,7 @@ import os
 import builtins
 from contextlib import contextmanager
 
-import nose
-from nose.tools import assert_equal, assert_true
+import pytest
 
 from xonsh.tools import swap
 from xonsh.shell import Shell
@@ -42,23 +41,19 @@ def a_replay(re_file):
 def test_echo():
     f = os.path.join(HISTDIR, 'echo.json')
     with a_replay(f) as hist:
-        yield assert_equal, 2, len(hist)
+        assert 2 == len(hist)
 
 
 @skip_if(ON_MAC)
 def test_reecho():
     f = os.path.join(HISTDIR, 'echo.json')
     with a_replay(f) as hist:
-        yield assert_equal, 2, len(hist)
+        assert 2 == len(hist)
 
 
 @skip_if(ON_MAC)
 def test_simple_python():
     f = os.path.join(HISTDIR, 'simple-python.json')
     with a_replay(f) as hist:
-        yield assert_equal, 4, len(hist)
-        yield assert_equal, "print('The Turtles')", hist.inps[0].strip()
-
-
-if __name__ == '__main__':
-    nose.runmodule()
+        assert 4 == len(hist)
+        assert "print('The Turtles')" == hist.inps[0].strip()
