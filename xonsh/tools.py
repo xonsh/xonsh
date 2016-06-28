b49a6934b04f3086ccc78d367295c37444c1de5a
Anthony Scopatz <scopatz@gmail.com>
2016-06-27 21:03:44 -0400

    path fix

--- xonsh/tools.py
+++ xonsh/tools.py
@@ -481,7 +481,9 @@ def _yield_accessible_unix_file_names(path):
 
 
 def _executables_in_posix(path):
-    if PYTHON_VERSION_INFO < (3, 5, 0):
+    if not os.path.exists(path):
+        return
+    elif PYTHON_VERSION_INFO < (3, 5, 0):
         for fname in os.listdir(path):
             fpath  = os.path.join(path, fname)
             if (os.path.exists(fpath) and os.access(fpath, os.X_OK) and \
