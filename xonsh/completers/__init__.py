a9e23d8dc706b6a453be75cd1f70d79987b43fed
Bernardas <bernardas.alisauskas@gmail.com>
2016-07-02 18:26:20 +0000

    add completer for vox

--- xonsh/completers/__init__.py
+++ xonsh/completers/__init__.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 from xonsh.completers.pip import complete_pip
 from xonsh.completers.man import complete_from_man
+from xonsh.completers.vox import complete_vox
 from xonsh.completers.bash import complete_from_bash
 from xonsh.completers.base import complete_base
 from xonsh.completers.path import complete_path
@@ -13,6 +14,7 @@ from xonsh.completers.commands import complete_skipper
 from xonsh.completers.completer import complete_completer
 
 completers = OrderedDict([
+    ('vox', complete_vox),
     ('python_mode', complete_python_mode),
     ('base', complete_base),
     ('completer', complete_completer),
