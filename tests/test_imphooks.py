0e7c8dc730f0245766306b65bf7b2f0f14cd6cef
laerus <laerusk@gmail.com>
2016-07-03 12:02:02 +0300

    'test_imphooks fixture fix'

--- tests/test_imphooks.py
+++ tests/test_imphooks.py
@@ -11,9 +11,9 @@ import builtins
 
 
 @pytest.yield_fixture(autouse=True)
-def imp_env():
+def imp_env(xonsh_execer):
     """Call `load_builtins with `xonsh_execer`"""
-    load_builtins(execer=Execer())
+    load_builtins(execer=xonsh_execer)
     builtins.__xonsh_env__ = Env({'PATH': [], 'PATHEXT': []})
     yield
     unload_builtins()
