7601c5746e6b972095cbcbb338a4334ee5b98dbe
Anthony Scopatz <scopatz@gmail.com>
2016-07-12 02:59:28 -0400

    vte_new_tab_cwd

--- xonsh/environ.py
+++ xonsh/environ.py
@@ -1170,7 +1170,13 @@ else:
     USER = 'USER'
 
 
-def new_vte_cwd():
+def vte_new_tab_cwd():
+    """This prints an escape squence that tells VTE terminals the hostname
+    and pwd. This should not be needed in most cases, but sometimes is for
+    certain Linux terminals that do not read the PWD from the environment
+    on startup. Note that this does not return a string, it simply prints
+    and flushes the escape sequence to stdout directly.
+    """
     env = builtins.__xonsh_env__
     t = '\033]7;file://{}{}\007'
     s = t.format(env.get('HOSTNAME'), env.get('PWD'))
@@ -1190,7 +1196,7 @@ FORMATTER_DICT = LazyObject(lambda: dict(
     branch_bg_color=branch_bg_color,
     current_job=_current_job,
     env_name=env_name,
-    new_vte_cwd=new_vte_cwd,
+    vte_new_tab_cwd=vte_new_tab_cwd,
     ), globals(), 'FORMATTER_DICT')
 
 
