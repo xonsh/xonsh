eb49536ae60bed258ccb4ddf8f01666f17d4fb4d
Bernardas <bernardas.alisauskas@gmail.com>
2016-07-12 21:01:51 +0000

    full refactor

--- xonsh/completers/vox.py
+++ xonsh/completers/vox.py
@@ -7,31 +7,33 @@ from xonsh.vox import Vox
 import os
 
 DEFAULT_ENV_HOME = os.path.expanduser('~/.virtualenvs')
+ALL_COMMANDS = LazyObject(lambda: [c[0].split()[1] for c in Vox.help_commands],
+                          globals(), 'ALL_COMMANDS')
 
 
 def complete_vox(prefix, line, begidx, endidx, ctx):
     """
     Completes Xonsh's Vox virtual environment manager
     """
-    if not line.startswith('vox'):
+    cases = [
+        not line.startswith('vox'),
+        len(line.split()) > 3,
+        len(line.split()) > 2 and line.endswith(' '),
+    ]
+    if any(cases):
         return
+
     to_list_when = ['vox activate ', 'vox remove ']
     if any(c in line for c in to_list_when):
         venv_home = builtins.__xonsh_env__.get('VIRTUALENV_HOME', DEFAULT_ENV_HOME)
         env_dirs = list(x.name for x in scandir(venv_home) if x.is_dir())
-        return set(env_dirs)
+        return env_dirs, len(prefix)
 
-    if (len(line.split()) > 1 and line.endswith(' ')) or len(line.split()) > 2:
-        # "vox new " -> no complete (note space)
+    if len(line.split()) > 1 and line.endswith(' '):  # "vox new "
         return
 
-    all_commands = [c[0].split()[1] for c in Vox.help_commands]
-    if prefix in all_commands:
-        # "vox new" -> suggest replacing new with other command (note no space)
-        return all_commands, len(prefix)
-    elif prefix:
-        # "vox n" -> suggest "new"
-        suggestions = [c for c in all_commands if c.startswith(prefix)]
+    if prefix not in ALL_COMMANDS:
+        suggestions = [c for c in ALL_COMMANDS if c.startswith(prefix)]
         if suggestions:
             return suggestions, len(prefix)
-    return all_commands, len(prefix)
+    return ALL_COMMANDS, len(prefix)
