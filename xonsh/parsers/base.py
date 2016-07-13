9424f850eb99d035a0a2806ce372d4bc1a873d10
Anthony Scopatz <scopatz@gmail.com>
2016-07-13 12:48:02 -0400

    Revert "minor updates"
    This reverts commit 206c174955ac09f062641f3547e5cf52cc9b3795.

--- xonsh/parsers/base.py
+++ xonsh/parsers/base.py
@@ -269,9 +269,8 @@ class BaseParser(object):
                            tabmodule=yacc_table)
         if not yacc_debug:
             yacc_kwargs['errorlog'] = yacc.NullLogger()
-        xonshdir = os.path.dirname(os.path.dirname(__file__))
-        if outputdir is None and not os.path.exists(os.path.join(xonshdir, 'parser_table.py')):
-            outputdir = xonshdir
+        if outputdir is None:
+            outputdir = os.path.dirname(os.path.dirname(__file__))
         yacc_kwargs['outputdir'] = outputdir
         self.parser = None
         YaccLoader(self, yacc_kwargs)
