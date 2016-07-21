657d927aa73bbc7db7b22d106d1db6510357e5bd
Jamie Bliss <astronouth7303@gmail.com>
2016-07-21 00:26:13 -0400

    First stab at argparse version. Doesn't actually work yet.

--- xontrib/vox.py
+++ xontrib/vox.py
@@ -1,77 +1,85 @@
 """Python virtual environment manager for xonsh."""
 
 import sys as _sys
+import argparse as _argparse
 import xontrib.voxapi as _voxapi
 
 class _VoxHandler:
     """Vox is a virtual environment manager for xonsh."""
+    parser = _argparse.ArgumentParser(prog='vox', description=__doc__)
+    subparsers = parser.add_subparsers(dest='command')
+
+    create = subparsers.add_parser('new', help='Create a new virtual environment')
+    create.add_argument('envs', metavar='ENV',
+                        help='The environments to create')
+
+    create.add_argument('--system-site-packages', default=False,
+                        action='store_true', dest='system_site',
+                        help='Give the virtual environment access to the '
+                             'system site-packages dir.')
+    from xonsh.platform import ON_WINDOWS
+    if ON_WINDOWS:
+        use_symlinks = False
+    else:
+        use_symlinks = True
+
+    group = create.add_mutually_exclusive_group()
+    group.add_argument('--symlinks', default=use_symlinks,
+                       action='store_true', dest='symlinks',
+                       help='Try to use symlinks rather than copies, '
+                            'when symlinks are not the default for '
+                            'the platform.')
+    group.add_argument('--copies', default=not use_symlinks,
+                       action='store_false', dest='symlinks',
+                       help='Try to use copies rather than symlinks, '
+                            'even when symlinks are the default for '
+                            'the platform.')
+    create.add_argument('--without-pip', dest='with_pip',
+                        default=True, action='store_false',
+                        help='Skips installing or upgrading pip in the '
+                             'virtual environment (pip is bootstrapped '
+                             'by default)')
+
+    group = create.add_mutually_exclusive_group()
+    activate = subparsers.add_parser('activate', aliases=['workon', 'enter'], help='Activate virtual environment')
+    create.add_argument('name', metavar='ENV', 
+                        help='The environment to activate')
+    subparsers.add_parser('deactivate', aliases=['exit'], help='Deactivate current virtual environment')
+    subparsers.add_parser('list', aliases=['ls'], help='List all available environments')
+    remove = subparsers.add_parser('remove', aliases=['rm', 'delete', 'del'], help='Remove virtual environment')
+    create.add_argument('envs', metavar='ENV', nargs='+',
+                        help='The environments to remove')
 
     def __init__(self):
-        """Ensure that $VIRTUALENV_HOME is defined and declare the available vox commands"""
-
         self.vox = _voxapi.Vox()
-
-        self.commands = {
-            ('new',): self.create_env,
-            ('activate', 'workon', 'enter'): self.activate_env,
-            ('deactivate', 'exit'): self.deactivate_env,
-            ('list', 'ls'): self.list_envs,
-            ('remove', 'rm', 'delete', 'del'): self.remove_envs,
-            ('help', '-h', '--help'): self.show_help
-        }
 
     def __call__(self, args, stdin=None):
         """Call the right handler method for a given command."""
 
-        if not args:
-            self.show_help()
-            return None
+        args = self.parser.parse_args(args)
+        getattr(self, 'cmd_'+args.command)(args, stdin)
 
-        command_name, params = args[0], args[1:]
-
-        try:
-            command = [
-                self.commands[aliases] for aliases in self.commands
-                if command_name in aliases
-            ][0]
-
-            command(*params)
-
-        except IndexError:
-            print('Command "%s" doesn\'t exist.\n' % command_name)
-            self.print_commands()
-
-    def create_env(self, name):
+    def cmd_create(self, args, stdin=None):
         """Create a virtual environment in $VIRTUALENV_HOME with python3's ``venv``.
-
-        Parameters
-        ----------
-        name : str
-            Virtual environment name
         """
         print('Creating environment...')
-        self.vox.create(name)
+        self.vox.create(args.name)
         msg = 'Environment {0!r} created. Activate it with "vox activate {0}".\n'
         print(msg.format(name))
 
-    def activate_env(self, name):
+    def cmd_activate(self, args, stdin=None):
         """Activate a virtual environment.
-
-        Parameters
-        ----------
-        name : str
-            Virtual environment name
         """
 
         try:
-            self.vox.activate(name)
+            self.vox.activate(args.name)
         except KeyError:
             print('This environment doesn\'t exist. Create it with "vox new %s".\n' % name, file=_sys.stderr)
             return None
         else:
             print('Activated "%s".\n' % name)
 
-    def deactivate_env(self):
+    def cmd_deactivate(self, args, stdin=None):
         """Deactive the active virtual environment."""
 
         if self.vox.active() is None:
@@ -80,11 +88,11 @@ class _VoxHandler:
         env_name = self.vox.deactivate()
         print('Deactivated "%s".\n' % env_name)
 
-    def list_envs(self):
+    def cmd_list(self, args, stdin=None):
         """List available virtual environments."""
 
         try:
-            envs = list(self.vox.keys())
+            envs = sorted(self.vox.keys())
         except PermissionError:
             print('No permissions on VIRTUALENV_HOME'.format(venv_home))
             return None
@@ -97,7 +105,7 @@ class _VoxHandler:
         print('\n'.join(envs))
 
 
-    def remove_envs(self, *names):
+    def cmd_remove(self, args, stdin=None):
         """Remove virtual environments.
 
         Parameters
@@ -107,7 +115,7 @@ class _VoxHandler:
         """
         for name in names:
             try:
-                del self.vox[name]
+                del self.vox[args.name]
             except _voxapi.EnvironmentInUse:
                 print('The "%s" environment is currently active. In order to remove it, deactivate it first with "vox deactivate %s".\n' % (name, name),
                       file=_sys.stderr)
@@ -115,36 +123,6 @@ class _VoxHandler:
             else:
                 print('Environment "%s" removed.' % name)
         print()
-
-    def show_help(self):
-        """Show help."""
-
-        print(self.__doc__, '\n')
-        self.print_commands()
-
-    @staticmethod
-    def print_commands():
-        """Print available vox commands."""
-
-        print("""Available commands:
-    vox new <env>
-        Create new virtual environment in $VIRTUALENV_HOME
-
-    vox activate (workon, enter) <env>
-        Activate virtual environment
-
-    vox deactivate (exit)
-        Deactivate current virtual environment
-
-    vox list (ls)
-        List all available environments
-
-    vox remove (rm, delete, del) <env> <env2> ...
-        Remove virtual environments
-
-    vox help (-h, --help)
-        Show help
-""")
 
     @classmethod
     def handle(cls, args, stdin=None):
