d6d61d803098dda8b6407016e3a8c00b8662d434
Anthony Scopatz <scopatz@gmail.com>
2016-07-22 20:55:30 -0400

    check encodings in xon.sh

--- scripts/xon.sh
+++ scripts/xon.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+# set locale if it is totally undefined
+if [ -z "${LC_ALL+x}" ] && [ -z "${LC_CTYPE+x}" ] && \
+   [ -z "${LANG+x}" ] && [ -z "${LANGUAGE+x}" ]; then
+  export LANG=C.UTF-8
+fi
+
+# run python
 /usr/bin/env PYTHONUNBUFFERED=1 python3 -u -m xonsh $@
