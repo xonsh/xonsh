7854fb0526c31bfe4242712747adb40299b31760
Gil Forsyth <gilforsyth@gmail.com>
2016-08-25 16:05:53 -0400

    use rpartition to split up prefix

--- xonsh/completers/python.py
+++ xonsh/completers/python.py
@@ -106,8 +106,7 @@ def attr_complete(prefix, ctx, filter_func):
             rpl = opt
         # note that prefix[:prelen-len(attr)] != prefix[:-len(attr)]
         # when len(attr) == 0.
-    #    comp = prefix[:prelen - len(attr)] + rpl
-        comp = rpl
+        comp = prefix[:prelen - len(attr)] + rpl
         attrs.add(comp)
     return attrs
 
