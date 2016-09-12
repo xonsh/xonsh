f83ff5b2c6a5f0d86ddc2653fbb6aa7905479b9f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-12 09:22:24 -0700

    Start a Python 3.6 parser.
    Write now not many changes, mostly async comprehension ast node takes a
    mandatory 4th keyword.

--- xonsh/parser.py
+++ xonsh/parser.py
@@ -6,8 +6,10 @@ from xonsh.platform import PYTHON_VERSION_INFO
 
 @lazyobject
 def Parser():
-    if PYTHON_VERSION_INFO < (3, 5, 0):
-        from xonsh.parsers.v34 import Parser as p
-    else:
+    if PYTHON_VERSION_INFO > (3, 6):
+        from xonsh.parsers.v36 import Parser as p
+    elif PYTHON_VERSION_INFO > (3, 5):
         from xonsh.parsers.v35 import Parser as p
+    else:
+        from xonsh.parsers.v34 import Parser as p
     return p
---
+++ xonsh/parsers/v36.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Implements the xonsh parser for Python v3.6."""
+import xonsh.ast as ast
+from xonsh.parsers.v35 import Parser as ThreeFiveParser
+from xonsh.parsers.base import store_ctx, ensure_has_elts
+
+
+class Parser(ThreeFiveParser):
+    """A Python v3.6 compliant parser for the xonsh language."""
+
+    def p_comp_for(self, p):
+        """comp_for : FOR exprlist IN or_test comp_iter_opt"""
+        targs, it, p5 = p[2], p[4], p[5]
+        if len(targs) == 1:
+            targ = targs[0]
+        else:
+            targ = ensure_has_elts(targs)
+        store_ctx(targ)
+        # only difference with base should be the is_async=0
+        comp = ast.comprehension(target=targ, iter=it, ifs=[], is_async=0)
+        comps = [comp]
+        p0 = {'comps': comps}
+        if p5 is not None:
+            comps += p5.get('comps', [])
+            comp.ifs += p5.get('if', [])
+        p[0] = p0
+
