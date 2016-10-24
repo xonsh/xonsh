cafa11563c520992e2eb5ee1ed6e209748d7698b
Morten Enemark Lund <melund@gmail.com>
2016-10-24 13:45:25 +0200

    Use ansi color names for default style
    This updates XONSH_BASE_STYLE to match the default style in pygments.
    Previously, XONSH_BASE_STYLE was only use if pygments was not installed.
    Now, XONSH_BASE_STYLE is always used if style is set to 'default'.
    In case pygment 2.2 is available a version style with color names
    is used (#ansidarkblue etc.).

--- xonsh/pyghooks.py
+++ xonsh/pyghooks.py
@@ -425,10 +425,13 @@ class XonshStyle(Style):
             value = 'default'
             builtins.__xonsh_env__['XONSH_COLOR_STYLE'] = value
         cmap = STYLES[value]
-        try:
-            self._smap = get_style_by_name(value)().styles.copy()
-        except (ImportError, pygments.util.ClassNotFound):
+        if value == 'default':
             self._smap = XONSH_BASE_STYLE.copy()
+        else:
+            try:
+                self._smap = get_style_by_name(value)().styles.copy()
+            except (ImportError, pygments.util.ClassNotFound):
+                self._smap = XONSH_BASE_STYLE.copy()
         compound = CompoundColorMap(ChainMap(self.trap, cmap, PTK_STYLE, self._smap))
         self.styles = ChainMap(self.trap, cmap, PTK_STYLE, self._smap, compound)
         self._style_name = value
@@ -469,55 +472,111 @@ def xonsh_style_proxy(styler):
     return XonshStyleProxy
 
 
-PTK_STYLE = {
-    Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
-    Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
-    Token.Menu.Completions.ProgressButton: 'bg:#003333',
-    Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
-    Token.AutoSuggestion: '#666666',
-    Token.Aborted: '#888888',
-}
+if hasattr(pygments.style, 'ansicolors'):
+    PTK_STYLE = {
+        Token.Menu.Completions.Completion.Current: 'bg:#ansiturquoise #ansiblack',
+        Token.Menu.Completions.Completion: 'bg:#ansiteal #ansiwhite',
+        Token.Menu.Completions.ProgressButton: 'bg:#ansidarkgray',
+        Token.Menu.Completions.ProgressBar: 'bg:#ansiteal',
+        Token.AutoSuggestion: '#ansidarkgray',
+        Token.Aborted: '#ansilightgray',
+    }
+else:
+    PTK_STYLE = {
+        Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
+        Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
+        Token.Menu.Completions.ProgressButton: 'bg:#003333',
+        Token.Menu.Completions.ProgressBar: 'bg:#ansiturquoise',
+        Token.AutoSuggestion: '#666666',
+        Token.Aborted: '#888888',
+    }
 
-XONSH_BASE_STYLE = LazyObject(lambda: {
-    Whitespace: '#008080',
-    Comment: 'underline',
-    Comment.Preproc: 'underline',
-    Keyword: 'bold',
-    Keyword.Pseudo: '#008000',
-    Keyword.Type: '',
-    Operator: '#008080',
-    Operator.Word: 'bold',
-    Name.Builtin: '',
-    Name.Function: '#000080',
-    Name.Class: 'bold',
-    Name.Namespace: 'bold',
-    Name.Exception: 'bold',
-    Name.Variable: '#008080',
-    Name.Constant: '#800000',
-    Name.Label: '#808000',
-    Name.Entity: 'bold',
-    Name.Attribute: '#008080',
-    Name.Tag: 'bold',
-    Name.Decorator: '#008080',
-    String: '',
-    String.Doc: 'underline',
-    String.Interpol: 'bold',
-    String.Escape: 'bold',
-    String.Regex: '',
-    String.Symbol: '',
-    String.Other: '#008000',
-    Number: '#800000',
-    Generic.Heading: 'bold',
-    Generic.Subheading: 'bold',
-    Generic.Deleted: '#800000',
-    Generic.Inserted: '#008000',
-    Generic.Error: 'bold',
-    Generic.Emph: 'underline',
-    Generic.Prompt: 'bold',
-    Generic.Output: '#008080',
-    Generic.Traceback: '#800000',
-    Error: '#800000',
+if hasattr(pygments.style, 'ansicolors'):
+    XONSH_BASE_STYLE = LazyObject(lambda: {
+        Whitespace: '#ansilightgray',
+        Comment: 'underline #ansiturquoise',
+        Comment.Preproc: 'underline #ansibrown',
+        Keyword: 'bold #ansidarkgreen',
+        Keyword.Pseudo: 'nobold',
+        Keyword.Type: 'nobold #ansidarkred',
+        Operator: '#ansidarkgray',
+        Operator.Word: 'bold #ansipurple',
+        Name.Builtin: '#ansidarkgreen',
+        Name.Function: '#ansiblue',
+        Name.Class: 'bold #ansiblue',
+        Name.Namespace: 'bold #ansiblue',
+        Name.Exception: 'bold #ansired',
+        Name.Variable: '#ansidarkblue',
+        Name.Constant: '#ansidarkred',
+        Name.Label: '#ansiyellow',
+        Name.Entity: 'bold #ansilightgray',
+        Name.Attribute: '#ansiyellow',
+        Name.Tag: 'bold #ansidarkgreen',
+        Name.Decorator: '#ansifuchsia',
+        String: '#ansired',
+        String.Doc: 'underline',
+        String.Interpol: 'bold #ansipurple',
+        String.Escape: 'bold #ansibrown',
+        String.Regex: '#ansipurple',
+        String.Symbol: '#ansibrown',
+        String.Other: '#ansidarkgreen',
+        Number: '#ansidarkgray',
+        Generic.Heading: 'bold #ansidarkblue',
+        Generic.Subheading: 'bold #ansipurple',
+        Generic.Deleted: '#ansidarkred',
+        Generic.Inserted: '#ansigreen',
+        Generic.Error: 'bold #ansired',
+        Generic.Emph: 'underline',
+        Generic.Prompt: 'bold #ansidarkblue',
+        Generic.Output: '#ansidarkblue',
+        Generic.Traceback: '#ansidarkblue',
+        Error: '#ansired',
+        }, globals(), 'XONSH_BASE_STYLE')
+else:
+    XONSH_BASE_STYLE = LazyObject(lambda: {
+        Whitespace: "#bbbbbb",
+        Comment: "italic #408080",
+        Comment.Preproc: "noitalic #BC7A00",
+        Keyword: "bold #008000",
+        Keyword.Pseudo: "nobold",
+        Keyword.Type:   "nobold #B00040",
+        Operator:   "#666666",
+        Operator.Word:  "bold #AA22FF",
+        Name.Builtin: "#008000",
+        Name.Function: "#0000FF",
+        Name.Class: "bold #0000FF",
+        Name.Namespace: "bold #0000FF",
+        Name.Exception: "bold #D2413A",
+        Name.Variable: "#19177C",
+        Name.Constant: "#880000",
+        Name.Label: "#A0A000",
+        Name.Entity: "bold #999999",
+        Name.Attribute: "#7D9029",
+        Name.Tag: "bold #008000",
+        Name.Decorator: "#AA22FF",
+        String: "#BA2121",
+        String.Doc: "italic",
+        String.Interpol: "bold #BB6688",
+        String.Escape: "bold #BB6622",
+        String.Regex: "#BB6688",
+        String.Symbol: "#19177C",
+        String.Other: "#008000",
+        Number: "#666666",
+        Generic.Heading: "bold #000080",
+        Generic.Subheading: "bold #800080",
+        Generic.Deleted: "#A00000",
+        Generic.Inserted: "#00A000",
+        Generic.Error: "#FF0000",
+        Generic.Emph: "italic",
+        Generic.Strong: "bold",
+        Generic.Prompt: "bold #000080",
+        Generic.Output: "#888",
+        Generic.Traceback: "#04D",
+        Error: "border:#FF0000"
     }, globals(), 'XONSH_BASE_STYLE')
+
+
+
 
 KNOWN_COLORS = LazyObject(lambda: frozenset([
     'BACKGROUND_BLACK',
