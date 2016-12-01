45670f1dd6a94ffd6b67378e1c629e16eff6bbae
Gil Forsyth <gilforsyth@gmail.com>
2016-12-01 12:41:19 -0500

    import xonsh.main to fix events docs generation
    sphinx will implicitly import main, but not before we call
    `make_events()`, so only `on_chdir` shows up on the events listing.
    explicitly importing `xonsh.main` fixes this up so all of the available
    events are documented properly.

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ from xonsh import __version__ as XONSH_VERSION
 from xonsh.environ import DEFAULT_DOCS, Env
 from xonsh.xontribs import xontrib_metadata
 from xonsh.events import events
+from xonsh import main
 from xonsh.commands_cache import CommandsCache
 
 sys.path.insert(0, os.path.dirname(__file__))
