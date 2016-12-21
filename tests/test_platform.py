fcc8f256f1a8e0c617a88cecaced6ec2dc7c75cc
Hugo Wang <w@mitnk.com>
2016-12-21 12:03:44 +0800

    Fixed xonfig raising error when xonsh is not installed from source

---
+++ tests/test_platform.py
@@ -0,0 +1,15 @@
+from contextlib import contextmanager
+from unittest.mock import MagicMock
+
+import builtins
+import xonsh.platform as xp
+
+
+def test_githash_value_error(monkeypatch):
+    @contextmanager
+    def mocked_open(*args):
+        yield MagicMock(read=lambda: 'abc123')
+    monkeypatch.setattr(builtins, 'open', mocked_open)
+    sha, date_ = xp.githash()
+    assert date_ is None
+    assert sha is None
