8cf955ecb5b88b530941ff5e6ccb983d11406976
Anthony Scopatz <scopatz@gmail.com>
2017-01-08 20:16:00 -0500

    source alias improvements

--- xonsh/aliases.py
+++ xonsh/aliases.py
@@ -255,18 +255,19 @@ def source_alias(args, stdin=None):
     env = builtins.__xonsh_env__
     encoding = env.get('XONSH_ENCODING')
     errors = env.get('XONSH_ENCODING_ERRORS')
-    for fname in args:
+    for i, fname in enumerate(args):
         fpath = fname
         if not os.path.isfile(fpath):
             fpath = locate_binary(fname)
-            if fpath is None:
+            if fpath is None and env.get('XONSH_DEBUG'):
                 print('source: {}: No such file'.format(fname), file=sys.stderr)
-                continue
+                break
         with open(fpath, 'r', encoding=encoding, errors=errors) as fp:
             src = fp.read()
         if not src.endswith('\n'):
             src += '\n'
-        builtins.execx(src, 'exec', builtins.__xonsh_ctx__)
+        with env.swap(ARGS=args[i+1:]):
+            builtins.execx(src, 'exec', builtins.__xonsh_ctx__, filename=fpath)
 
 
 def source_cmd(args, stdin=None):
