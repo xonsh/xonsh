4ca41e7aa18577977d606647fac9927478b516e5
Anthony Scopatz <scopatz@gmail.com>
2017-02-04 16:07:25 -0500

    ON_BSD fix

--- xonsh/platform.py
+++ xonsh/platform.py
@@ -52,9 +52,12 @@ ON_FREEBSD = LazyBool(lambda: (sys.platform.startswith('freebsd')),
 ON_NETBSD = LazyBool(lambda: (sys.platform.startswith('netbsd')),
                      globals(), 'ON_NETBSD')
 """``True`` if on a NetBSD operating system, else ``False``."""
-ON_BSD = LazyBool(lambda: ON_FREEBSD or ON_NETBSD,
-                  globals(), 'ON_BSD')
-"""``True`` if on a BSD operating system, else ``False``."""
+
+
+@lazybool
+def ON_BSD():
+    """``True`` if on a BSD operating system, else ``False``."""
+    return bool(ON_FREEBSD) or bool(ON_NETBSD)
 
 
 @lazybool
