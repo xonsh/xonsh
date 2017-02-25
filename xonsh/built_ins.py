d45c355b627399c1b2da076d3be92aab3efdc06e
adam j hartz <hartz@mit.edu>
2017-02-25 16:12:43 -0500

    allow generators and bare tuples in @(...)

--- xonsh/built_ins.py
+++ xonsh/built_ins.py
@@ -885,10 +885,11 @@ def list_of_strs_or_callables(x):
     """Ensures that x is a list of strings or functions"""
     if isinstance(x, str) or callable(x):
         rtn = [x]
-    elif isinstance(x, cabc.Sequence):
-        rtn = [i if isinstance(i, str) or callable(i) else str(i) for i in x]
     else:
-        rtn = [str(x)]
+        try:
+            rtn = [i if isinstance(i, str) or callable(i) else str(i) for i in x]
+        except:
+            rtn = [str(x)]
     return rtn
 
 
