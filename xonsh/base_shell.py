4f3910fe3243af86c7cdd61179dc7ca7afd12695
Anthony Scopatz <scopatz@gmail.com>
2017-02-26 12:49:26 -0500

    No longer crash when PWD is removed

--- xonsh/base_shell.py
+++ xonsh/base_shell.py
@@ -375,8 +375,26 @@ class BaseShell(object):
     def _fix_cwd(self):
         """Check if the cwd changed out from under us."""
         env = builtins.__xonsh_env__
-        cwd = os.getcwd()
-        if 'PWD' not in env:
+        try:
+            cwd = os.getcwd()
+        except (FileNotFoundError, OSError):
+            cwd = None
+        if cwd is None:
+            # directory has been deleted out from under us, most likely
+            pwd = env.get('PWD', None)
+            if pwd is None:
+                # we have no idea where we are
+                env['PWD'] = '<invalid directory>'
+            elif os.path.isdir(pwd):
+                # unclear why os.getcwd() failed. do nothing.
+                pass
+            else:
+                # OK PWD is really gone.
+                msg = '{UNDERLINE_INTENSE_WHITE}{BACKGROUND_INTENSE_BLACK}'
+                msg += "xonsh: working directory does not exist: " + pwd
+                msg += '{NO_COLOR}'
+                self.print_color(msg, file=sys.stderr)
+        elif 'PWD' not in env:
             # $PWD is missing from env, recreate it
             env['PWD'] = cwd
         elif os.path.realpath(cwd) != os.path.realpath(env['PWD']):
--- xonsh/pyghooks.py
+++ xonsh/pyghooks.py
@@ -34,7 +34,10 @@ load_module_in_background('pkg_resources', debug='XONSH_DEBUG',
 
 
 def _command_is_valid(cmd):
-    cmd_abspath = os.path.abspath(os.path.expanduser(cmd))
+    try:
+        cmd_abspath = os.path.abspath(os.path.expanduser(cmd))
+    except (FileNotFoundError, OSError):
+        return False
     return cmd in builtins.__xonsh_commands_cache__ or \
         (os.path.isfile(cmd_abspath) and os.access(cmd_abspath, os.X_OK))
 
