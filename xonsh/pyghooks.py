f70a2b67a08478ffa51e9321d68be9316e2b0892
Anthony Scopatz <scopatz@gmail.com>
2017-02-26 18:21:39 -0500

    try to replace a god

--- xonsh/pyghooks.py
+++ xonsh/pyghooks.py
@@ -45,7 +45,7 @@ def _command_is_valid(cmd):
 def _command_is_autocd(cmd):
     if not builtins.__xonsh_env__.get('AUTO_CD', False):
         return False
-    tyr:
+    try:
         cmd_abspath = os.path.abspath(os.path.expanduser(cmd))
     except (FileNotFoundError, OSError):
         return False
