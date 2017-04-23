732ef3e18673c4ee456b70c42610476ac6dd377d
Hugo Wang <w@mitnk.com>
2017-04-23 14:34:39 +0800

    removed unused imports

--- xonsh/commands_cache.py
+++ xonsh/commands_cache.py
@@ -7,12 +7,11 @@ and returns whethere or not the process can be run in the background (returns
 True) or must be run the foreground (returns False).
 """
 import os
-import time
 import builtins
 import argparse
 import collections.abc as cabc
 
-from xonsh.platform import ON_WINDOWS, ON_POSIX, pathbasename
+from xonsh.platform import ON_WINDOWS, pathbasename
 from xonsh.tools import executables_in
 from xonsh.lazyasd import lazyobject
 
