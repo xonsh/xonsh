cf973c202be3598123f8bb86becc65b6a2beec94
Gil Forsyth <Gilbert.Forsyth@capitalone.com>
2017-11-15 13:03:45 -0500

    Escape regex in path completion

---
+++ news/escape_regex.rst
@@ -0,0 +1,14 @@
+**Added:** None
+
+**Changed:** None
+
+**Deprecated:** None
+
+**Removed:** None
+
+**Fixed:**
+
+* Escape regex characters in ``path_complete`` to avoid regex parsing errors for
+  certain combinations of characters in path completer
+
+**Security:** None
--- tests/test_path_completers.py
+++ tests/test_path_completers.py
@@ -1,5 +1,20 @@
+import pytest
+
+from xonsh.environ import Env
 import xonsh.completers.path as xcp
+
 
 def test_pattern_need_quotes():
     # just make sure the regex compiles
     xcp.PATTERN_NEED_QUOTES.match('')
+
+
+def test_complete_path(xonsh_builtins):
+    xonsh_builtins.__xonsh_env__ = {'CASE_SENSITIVE_COMPLETIONS': False,
+                                    'GLOB_SORTED': True,
+                                    'SUBSEQUENCE_PATH_COMPLETION': False,
+                                    'FUZZY_PATH_COMPLETION': False,
+                                    'SUGGEST_THRESHOLD': 3,
+                                    'CDPATH': set(),
+    }
+    xcp.complete_path('[1-0.1]', '[1-0.1]', 0, 7, dict())
