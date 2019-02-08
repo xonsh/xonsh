d4b8cca0f1e800990c9bf3c133aa0b29b2c2c43d
Anthony Scopatz <scopatz@gmail.com>
2019-02-08 18:16:26 -0500

    LsColors seems to work

--- xonsh/environ.py
+++ xonsh/environ.py
@@ -11,6 +11,7 @@ import warnings
 import contextlib
 import collections
 import collections.abc as cabc
+import subprocess
 
 from xonsh import __version__ as XONSH_VERSION
 from xonsh.lazyasd import LazyObject, lazyobject
@@ -77,6 +78,11 @@ from xonsh.tools import (
     is_str_str_dict,
     to_str_str_dict,
     dict_to_str,
+)
+from xonsh.ansi_colors import (
+    ansi_color_escape_code_to_name,
+    ansi_reverse_style,
+    ansi_style_by_name,
 )
 import xonsh.prompt.base as prompt
 
@@ -157,6 +163,252 @@ def to_debug(x):
         builtins.__xonsh__.execer.debug_level = val
     return val
 
+
+#
+# $LS_COLORS tools
+#
+
+
+class LsColors(cabc.MutableMapping):
+    """Helps convert to/from $LS_COLORS format, respecting the xonsh color style.
+    This accepts the same inputs as dict().
+    """
+
+    default_settings = {
+        "*.7z": ("BOLD_RED",),
+        "*.Z": ("BOLD_RED",),
+        "*.aac": ("CYAN",),
+        "*.ace": ("BOLD_RED",),
+        "*.alz": ("BOLD_RED",),
+        "*.arc": ("BOLD_RED",),
+        "*.arj": ("BOLD_RED",),
+        "*.asf": ("BOLD_PURPLE",),
+        "*.au": ("CYAN",),
+        "*.avi": ("BOLD_PURPLE",),
+        "*.bmp": ("BOLD_PURPLE",),
+        "*.bz": ("BOLD_RED",),
+        "*.bz2": ("BOLD_RED",),
+        "*.cab": ("BOLD_RED",),
+        "*.cgm": ("BOLD_PURPLE",),
+        "*.cpio": ("BOLD_RED",),
+        "*.deb": ("BOLD_RED",),
+        "*.dl": ("BOLD_PURPLE",),
+        "*.dwm": ("BOLD_RED",),
+        "*.dz": ("BOLD_RED",),
+        "*.ear": ("BOLD_RED",),
+        "*.emf": ("BOLD_PURPLE",),
+        "*.esd": ("BOLD_RED",),
+        "*.flac": ("CYAN",),
+        "*.flc": ("BOLD_PURPLE",),
+        "*.fli": ("BOLD_PURPLE",),
+        "*.flv": ("BOLD_PURPLE",),
+        "*.gif": ("BOLD_PURPLE",),
+        "*.gl": ("BOLD_PURPLE",),
+        "*.gz": ("BOLD_RED",),
+        "*.jar": ("BOLD_RED",),
+        "*.jpeg": ("BOLD_PURPLE",),
+        "*.jpg": ("BOLD_PURPLE",),
+        "*.lha": ("BOLD_RED",),
+        "*.lrz": ("BOLD_RED",),
+        "*.lz": ("BOLD_RED",),
+        "*.lz4": ("BOLD_RED",),
+        "*.lzh": ("BOLD_RED",),
+        "*.lzma": ("BOLD_RED",),
+        "*.lzo": ("BOLD_RED",),
+        "*.m2v": ("BOLD_PURPLE",),
+        "*.m4a": ("CYAN",),
+        "*.m4v": ("BOLD_PURPLE",),
+        "*.mid": ("CYAN",),
+        "*.midi": ("CYAN",),
+        "*.mjpeg": ("BOLD_PURPLE",),
+        "*.mjpg": ("BOLD_PURPLE",),
+        "*.mka": ("CYAN",),
+        "*.mkv": ("BOLD_PURPLE",),
+        "*.mng": ("BOLD_PURPLE",),
+        "*.mov": ("BOLD_PURPLE",),
+        "*.mp3": ("CYAN",),
+        "*.mp4": ("BOLD_PURPLE",),
+        "*.mp4v": ("BOLD_PURPLE",),
+        "*.mpc": ("CYAN",),
+        "*.mpeg": ("BOLD_PURPLE",),
+        "*.mpg": ("BOLD_PURPLE",),
+        "*.nuv": ("BOLD_PURPLE",),
+        "*.oga": ("CYAN",),
+        "*.ogg": ("CYAN",),
+        "*.ogm": ("BOLD_PURPLE",),
+        "*.ogv": ("BOLD_PURPLE",),
+        "*.ogx": ("BOLD_PURPLE",),
+        "*.opus": ("CYAN",),
+        "*.pbm": ("BOLD_PURPLE",),
+        "*.pcx": ("BOLD_PURPLE",),
+        "*.pgm": ("BOLD_PURPLE",),
+        "*.png": ("BOLD_PURPLE",),
+        "*.ppm": ("BOLD_PURPLE",),
+        "*.qt": ("BOLD_PURPLE",),
+        "*.ra": ("CYAN",),
+        "*.rar": ("BOLD_RED",),
+        "*.rm": ("BOLD_PURPLE",),
+        "*.rmvb": ("BOLD_PURPLE",),
+        "*.rpm": ("BOLD_RED",),
+        "*.rz": ("BOLD_RED",),
+        "*.sar": ("BOLD_RED",),
+        "*.spx": ("CYAN",),
+        "*.svg": ("BOLD_PURPLE",),
+        "*.svgz": ("BOLD_PURPLE",),
+        "*.swm": ("BOLD_RED",),
+        "*.t7z": ("BOLD_RED",),
+        "*.tar": ("BOLD_RED",),
+        "*.taz": ("BOLD_RED",),
+        "*.tbz": ("BOLD_RED",),
+        "*.tbz2": ("BOLD_RED",),
+        "*.tga": ("BOLD_PURPLE",),
+        "*.tgz": ("BOLD_RED",),
+        "*.tif": ("BOLD_PURPLE",),
+        "*.tiff": ("BOLD_PURPLE",),
+        "*.tlz": ("BOLD_RED",),
+        "*.txz": ("BOLD_RED",),
+        "*.tz": ("BOLD_RED",),
+        "*.tzo": ("BOLD_RED",),
+        "*.tzst": ("BOLD_RED",),
+        "*.vob": ("BOLD_PURPLE",),
+        "*.war": ("BOLD_RED",),
+        "*.wav": ("CYAN",),
+        "*.webm": ("BOLD_PURPLE",),
+        "*.wim": ("BOLD_RED",),
+        "*.wmv": ("BOLD_PURPLE",),
+        "*.xbm": ("BOLD_PURPLE",),
+        "*.xcf": ("BOLD_PURPLE",),
+        "*.xpm": ("BOLD_PURPLE",),
+        "*.xspf": ("CYAN",),
+        "*.xwd": ("BOLD_PURPLE",),
+        "*.xz": ("BOLD_RED",),
+        "*.yuv": ("BOLD_PURPLE",),
+        "*.z": ("BOLD_RED",),
+        "*.zip": ("BOLD_RED",),
+        "*.zoo": ("BOLD_RED",),
+        "*.zst": ("BOLD_RED",),
+        "bd": ("BACKGROUND_BLACK", "YELLOW"),
+        "ca": ("BLACK", "BACKGROUND_RED"),
+        "cd": ("BACKGROUND_BLACK", "YELLOW"),
+        "di": ("BOLD_BLUE",),
+        "do": ("BOLD_PURPLE",),
+        "ex": ("BOLD_GREEN",),
+        "ln": ("BOLD_CYAN",),
+        "mh": ("NO_COLOR",),
+        "mi": ("NO_COLOR",),
+        "or": ("BACKGROUND_BLACK", "RED"),
+        "ow": ("BLUE", "BACKGROUND_GREEN"),
+        "pi": ("BACKGROUND_BLACK", "YELLOW"),
+        "rs": ("NO_COLOR",),
+        "sg": ("BLACK", "BACKGROUND_YELLOW"),
+        "so": ("BOLD_PURPLE",),
+        "st": ("WHITE", "BACKGROUND_BLUE"),
+        "su": ("WHITE", "BACKGROUND_RED"),
+        "tw": ("BLACK", "BACKGROUND_GREEN"),
+    }
+
+    def __init__(self, *args, **kwargs):
+        self._d = dict(*args, **kwargs)
+        self._style = self._style_name = None
+        self._detyped = None
+
+    def __getitem__(self, key):
+        return self._d[key]
+
+    def __setitem__(self, key, value):
+        self._detyped = None
+        self._d[key] = value
+
+    def __delitem__(self, key):
+        self._detyped = None
+        del self._d[key]
+
+    def __len__(self):
+        return len(self._d)
+
+    def __iter__(self):
+        yield from self._d
+
+    def detype(self):
+        """De-types the instance, allowing it to be exported to the environment."""
+        style = self.style
+        if self._detyped is None:
+            self._detyped = ":".join(
+                [
+                    key + "=" + ";".join([style[v] for v in val])
+                    for key, val in sorted(self._d.items())
+                ]
+            )
+        return self._detyped
+
+    @property
+    def style_name(self):
+        """Current XONSH_COLOR_STYLE value"""
+        env = builtins.__xonsh__.env
+        env_style_name = env.get("XONSH_COLOR_STYLE")
+        if self._style_name is None or self._style_name != env_style_name:
+            self._style_name = env_style_name
+            self._style = self._dtyped = None
+        return self._style_name
+
+    @property
+    def style(self):
+        """The ANSI color style for the current XONSH_COLOR_STYLE"""
+        style_name = self.style_name
+        if self._style is None:
+            self._style = ansi_style_by_name(style_name)
+            self._detyped = None
+        return self._style
+
+    @classmethod
+    def fromstring(cls, s):
+        """Creates a new instance of the LsColors class from a colon-separated
+        string of dircolor-valid keys to ANSI color escape sequences.
+        """
+        obj = cls()
+        # string inputs always use default codes, so translating into
+        # xonsh names should be done from defaults
+        reversed_default = ansi_reverse_style(style="default")
+        data = {}
+        for item in s.split(":"):
+            key, eq, esc = item.partition("=")
+            if not eq:
+                # not a valid item
+                continue
+            data[key] = ansi_color_escape_code_to_name(
+                esc, reversed_style=reversed_default
+            )
+        obj._d = data
+        return obj
+
+    @classmethod
+    def fromdircolors(cls, filename=None):
+        """Constructs an LsColors instance by running dircolors.
+        If a filename is provided, it is passed down to the dircolors command.
+        """
+        # assemble command
+        cmd = ["dircolors", "-b"]
+        if filename is not None:
+            cmd.append(filename)
+        # get env
+        if hasattr(builtins, "__xonsh__"):
+            denv = builtins.__xonsh__.env.detype()
+        else:
+            denv = None
+        # run dircolors
+        try:
+            out = subprocess.check_output(cmd, env=denv, universal_newlines=True)
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            return cls(cls.default_settings)
+        s = out.splitlines()[0]
+        _, _, s = s.partition("'")
+        s, _, _ = s.rpartition("'")
+        return cls.fromstring(s)
+
+
+#
+# Ensurerers
+#
 
 Ensurer = collections.namedtuple("Ensurer", ["validate", "convert", "detype"])
 Ensurer.__doc__ = """Named tuples whose elements are functions that
