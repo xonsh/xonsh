7d97459836913a8f1186def24a91699a1a6c20cd
virus <virusbb001a@gmail.com>
2019-05-22 21:50:41 +0900

    use isinstance for check type

--- tests/test_commands_cache.py
+++ tests/test_commands_cache.py
@@ -23,7 +23,7 @@ def test_commands_cache_lazy(xonsh_builtins):
 def test_predict_threadable_unknown_command(xonsh_builtins):
     cc = CommandsCache()
     result = cc.predict_threadable(["command_should_not_found"])
-    assert type(result) == bool
+    assert isinstance(result, bool)
 
 
 TRUE_SHELL_ARGS = [
