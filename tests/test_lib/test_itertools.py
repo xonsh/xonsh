ba179145249e5377a9f45d9cbd0261eeda2a468d
Christopher J. Wright <cjwright4242@gmail.com>
2019-09-11 11:56:38 -0400

    add test

---
+++ tests/test_lib/test_itertools.py
@@ -0,0 +1,17 @@
+from xonsh.lib.itertools import as_iterable
+
+
+def test_single():
+    assert as_iterable(1) == (1,)
+
+
+def test_list():
+    assert as_iterable([1, 2, 3]) == [1, 2, 3]
+
+
+def test_string():
+    assert as_iterable("my string") == ("my string",)
+
+
+def test_dict():
+    assert as_iterable({"a": 1}) == ({"a": 1},)
