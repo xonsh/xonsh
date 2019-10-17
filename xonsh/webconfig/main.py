7e5918bc7e2e543f6da967ec26963c85ce161b83
Anthony Scopatz <scopatz@gmail.com>
2019-10-17 17:56:46 -0400

    added render infra

--- xonsh/webconfig/main.py
+++ xonsh/webconfig/main.py
@@ -10,6 +10,55 @@ from pprint import pprint
 from argparse import ArgumentParser
 
 
+RENDERERS = []
+
+def renderer(f):
+    """Adds decorated function to renderers list."""
+    RENDERERS.append(f)
+
+
+@renderer
+def prompt(config):
+    return ["$PROMPT = {!r}".format(config['prompt'])]
+
+
+def config_to_xonsh(config,
+                        prefix="# XONSH WEBCONFIG START",
+                        suffix="# XONSH WEBCONFIG END",
+):
+    """Turns config dict into xonsh code (str)."""
+    lines = [prefix]
+    for func in RENDERERS:
+        lines.extend(func(config))
+    lines.append(suffix)
+    return "\n".join(lines)
+
+
+def insert_into_xonshrc(config, xonshrc="~/.xonshrc",
+                        prefix="# XONSH WEBCONFIG START",
+                        suffix="# XONSH WEBCONFIG END",
+                        ):
+    """Places a config dict into the xonshrc."""
+    # get current contents
+    fname = os.path.expanduser(xonshrc)
+    if os.path.isfile(fname):
+        with open(fname, 'r') as f:
+            s = f.read()
+        before, _, s = s.partition(prefix)
+        _, _, after = s.partition(suffix)
+    else:
+        before = after = ""
+        dname = os.path.dirname(fname)
+        if dname:
+            os.makedirs(dname, exist_ok=True)
+    # compute new values
+    new = config_to_xonsh(config, prefix=prefix, suffix=suffix)
+    # write out the file
+    with open(fname, "w") as f:
+        f.write(before + new + after)
+    return fname
+
+
 class XonshConfigHTTPRequestHandler(server.SimpleHTTPRequestHandler):
 
     def _set_headers(self):
@@ -25,6 +74,8 @@ class XonshConfigHTTPRequestHandler(server.SimpleHTTPRequestHandler):
         config = json.loads(post_body)
         print("Web Config Values:")
         pprint(config)
+        fname = insert_into_xonshrc(config)
+        print("Wrote out to " + fname)
         self.wfile.write(b"received post request:<br>" + post_body)
 
 
