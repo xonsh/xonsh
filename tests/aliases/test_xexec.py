3a4eb26215c914f5cb3aecc5aa6a70114f5a3e59
Jerzy Drozdz <jerzy.drozdz@gmail.com>
2020-03-01 09:51:31 +0100

    Testing non existent command with mocked execvpe

--- tests/aliases/test_xexec.py
+++ tests/aliases/test_xexec.py
@@ -8,7 +8,7 @@ from xonsh.aliases import xexec
 
 @pytest.fixture
 def mockexecvpe(monkeypatch):
-    def mocked_execvpe(command, args, env):
+    def mocked_execvpe(_command, _args, _env):
         pass
 
     monkeypatch.setattr(os, "execvpe", mocked_execvpe)
@@ -24,23 +24,30 @@ def test_missing_command(mockexecvpe):
     assert xexec(["-l"]) == (None, "xonsh: exec: no command specified\n", 1)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7) and sys.platform.startswith("win"),
-                    reason="Python <= 3.6 on Windows returns different error message")
-def test_command_not_found():
+# @pytest.mark.skipif(sys.version_info < (3, 7) and sys.platform.startswith("win"),
+#                     reason="Python <= 3.6 on Windows returns different error message")
+def test_command_not_found(monkeypatch):
+
+    dummy_error_msg = "This is dummy error message, file not found or something like that"
     command = "non_existing_command"
+
+    def mocked_execvpe(_command, _args, _env):
+        raise FileNotFoundError(2, dummy_error_msg)
+    monkeypatch.setattr(os, "execvpe", mocked_execvpe)
+
     assert xexec([command]) == (None,
-                                "xonsh: exec: file not found: {}: {}" "\n".format("No such file or directory", command),
+                                "xonsh: exec: file not found: {}: {}" "\n".format(dummy_error_msg, command),
                                 1)
 
 
-@pytest.mark.skipif((sys.version_info > (3, 6) and sys.platform.startswith("win")) or not sys.platform.startswith("win"),
-                    reason="Python > 3.6 on Windows returns unified error message")
-def test_win_py36_command_not_found():
-    command = "non_existing_command"
-    assert xexec([command]) == (None,
-                                "xonsh: exec: file not found: {}: {}" "\n".
-                                format("The system cannot find the path specified", command),
-                                1)
+# @pytest.mark.skipif((sys.version_info > (3, 6) and sys.platform.startswith("win")) or not sys.platform.startswith("win"),
+#                     reason="Python > 3.6 on Windows returns unified error message")
+# def test_win_py36_command_not_found():
+#     command = "non_existing_command"
+#     assert xexec([command]) == (None,
+#                                 "xonsh: exec: file not found: {}: {}" "\n".
+#                                 format("The system cannot find the path specified", command),
+#                                 1)
 
 
 def test_help(mockexecvpe):
