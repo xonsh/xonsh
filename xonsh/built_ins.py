403423ea557166f1e4db92e17c43ba1ee9e46479
Wendell Turner <skyking@ilikecarrots.com>
2020-09-13 23:29:29 -0400

    formatting

--- xonsh/built_ins.py
+++ xonsh/built_ins.py
@@ -216,7 +216,7 @@ def get_script_subproc_command(fname, args):
             raise PermissionError
         # explicitly look at all PATH entries for cmd
         w_path = os.getenv("PATH").split(':')
-        w_fpath = list(map(lambda p: p + os.sep + fname,w_path))
+        w_fpath = list(map(lambda p: p + os.sep + fname, w_path))
         if not any(list(map(lambda c: os.access(c, os.X_OK), w_fpath))):
             raise PermissionError
     if ON_POSIX and not os.access(fname, os.R_OK):
