ea5a8fcdbf10b7e6b60bc30b2b55e5d33db6616d
Anthony Scopatz <scopatz@gmail.com>
2020-10-20 11:31:46 -0500

    dep warnings

--- xonsh/__init__.py
+++ xonsh/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.9.24"
 
 # amalgamate exclude jupyter_kernel parser_table parser_test_table pyghooks
 # amalgamate exclude winutils wizard pytest_plugin fs macutils pygments_cache
-# amalgamate exclude jupyter_shell
+# amalgamate exclude jupyter_shell proc
 import os as _os
 
 if _os.getenv("XONSH_DEBUG", ""):
