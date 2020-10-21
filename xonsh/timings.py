867904e5af99f4771b1b05d373a9f8aa78ec8123
a <a>
2020-10-21 18:32:11 +0300

    on_pre_prompt

--- xonsh/timings.py
+++ xonsh/timings.py
@@ -310,6 +310,11 @@ def setup_timings(argv):
             global _timings
             _timings["on_chdir"] = clock()
 
+        @events.on_pre_prompt
+        def timing_on_pre_prompt(**kw):
+            global _timings
+            _timings["on_pre_prompt"] = clock()
+
         @events.on_post_prompt
         def timing_on_post_prompt(**kw):
             global _timings
