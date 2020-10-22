8cdce2d5e20cdbc18f805a26160cf2e7f3b0abd1
Tuux <tuxa@rtnp.org>
2020-10-22 13:39:56 +0200

    RISC OS only comment thing

--- xonsh/xoreutils/uptime.py
+++ xonsh/xoreutils/uptime.py
@@ -10,6 +10,9 @@ to standard output.
 
 File forked from: https://github.com/Cairnarvon/uptime
 """
+import sys
+import time
+
 try:
     import locale
 
@@ -29,25 +32,16 @@ try:
 except ImportError:
     pass
 
-import sys
-import time
-
 try:
     from datetime import datetime
 except ImportError:
     datetime = None
 
-try:
-    # RISC OS only.
-    import swi
-except ImportError:
-    pass
-
-try:
-    # Mac OS, Python <3 only.
-    import MacOS
-except ImportError:
-    pass
+# RISC OS only but actually unsupported by xonsh
+# try:
+#     import swi
+# except ImportError:
+#     pass
 
 try:
     from _posix import _uptime_posix, _uptime_osx
@@ -349,37 +343,37 @@ def uptime():
         return time.time() - __boottime
 
     return (
-        {
-            "amiga": _uptime_amiga,
-            "aros12": _uptime_amiga,
-            "beos5": _uptime_beos,
-            "cygwin": _uptime_linux,
-            "darwin": _uptime_osx,
-            "haiku": _uptime_beos,
-            "haiku1": _uptime_beos,
-            "linux": _uptime_linux,
-            "linux-armv71": _uptime_linux,
-            "linux2": _uptime_linux,
-            "mac": _uptime_mac,
-            "minix3": _uptime_minix,
-            "riscos": _uptime_riscos,
-            "sunos5": _uptime_solaris,
-            "syllable": _uptime_syllable,
-            "win32": _uptime_windows,
-            "wince": _uptime_windows,
-        }.get(sys.platform, _uptime_bsd)()
-        or _uptime_bsd()
-        or _uptime_plan9()
-        or _uptime_linux()
-        or _uptime_windows()
-        or _uptime_solaris()
-        or _uptime_beos()
-        or _uptime_amiga()
-        or _uptime_riscos()
-        or _uptime_posix()
-        or _uptime_syllable()
-        or _uptime_mac()
-        or _uptime_osx()
+            {
+                "amiga": _uptime_amiga,
+                "aros12": _uptime_amiga,
+                "beos5": _uptime_beos,
+                "cygwin": _uptime_linux,
+                "darwin": _uptime_osx,
+                "haiku": _uptime_beos,
+                "haiku1": _uptime_beos,
+                "linux": _uptime_linux,
+                "linux-armv71": _uptime_linux,
+                "linux2": _uptime_linux,
+                "mac": _uptime_mac,
+                "minix3": _uptime_minix,
+                "riscos": _uptime_riscos,
+                "sunos5": _uptime_solaris,
+                "syllable": _uptime_syllable,
+                "win32": _uptime_windows,
+                "wince": _uptime_windows,
+            }.get(sys.platform, _uptime_bsd)()
+            or _uptime_bsd()
+            or _uptime_plan9()
+            or _uptime_linux()
+            or _uptime_windows()
+            or _uptime_solaris()
+            or _uptime_beos()
+            or _uptime_amiga()
+            or _uptime_riscos()
+            or _uptime_posix()
+            or _uptime_syllable()
+            or _uptime_mac()
+            or _uptime_osx()
     )
 
 
