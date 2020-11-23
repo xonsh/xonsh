7fb4d06cb21b35e8cf830773ba4e40f1960a9155
David Strobach <lalochcz@gmail.com>
2020-11-23 22:23:46 +0100

    ulimit: Change default to access soft limits (#3990)
    * ulimit: Change default to access soft limits
    
    It makes more sense to manipulate "soft" limits by default, because
    those are the limits intended to be user-adjustable. This is also
    what bash does. Originally, I made ulimit to default to "hard" limits,
    because I was for some reason convinced that it's what bash does.
    
    * Changelog entry

--- xonsh/xoreutils/ulimit.py
+++ xonsh/xoreutils/ulimit.py
@@ -195,8 +195,8 @@ def _ul_parse_args(args, stderr):
         long_opts[_UL_RES[k][1]] = k
 
     actions = []
-    # mimic bash and default to 'hard' limits
-    res_type = _UL_HARD
+    # mimic bash and default to 'soft' limits
+    res_type = _UL_SOFT
     for arg in args:
         if arg in long_opts:
             opt = long_opts[arg]
