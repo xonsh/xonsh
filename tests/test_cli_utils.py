321890a86d8cf7eb25b25cfe6b5e47b13ffd89c3
Noortheen Raja <jnoortheen@gmail.com>
2020-12-16 02:47:20 +0530

    fix: testing get-doc cross-platform

--- tests/test_cli_utils.py
+++ tests/test_cli_utils.py
@@ -22,17 +22,14 @@ def func_with_doc(param: str, multi: str) -> str:
 
 
 def test_get_doc_param():
-    assert (
-        cli_utils.get_doc(func_with_doc)
-        == """\
-func doc
-multi-line
-"""
-    )
-    assert cli_utils.get_doc(func_with_doc, "param") == "param doc"
-    assert (
-        cli_utils.get_doc(func_with_doc, "multi")
-        == """\
-param doc
-    multi line"""
-    )
+    assert cli_utils.get_doc(func_with_doc).splitlines() == [
+        "func doc",
+        "multi-line",
+    ]
+    assert cli_utils.get_doc(func_with_doc, "param").splitlines() == [
+        "param doc",
+    ]
+    assert cli_utils.get_doc(func_with_doc, "multi").splitlines() == [
+        "param doc",
+        "    multi line",
+    ]
