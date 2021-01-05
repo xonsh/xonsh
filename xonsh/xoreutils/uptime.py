e0be7f4d9196193192c1103009553aec75ec5650
Konstantin Schukraft <konstantin@schukraft.org>
2021-01-05 22:07:22 +0100

    sysctlbyname needs bytes, not unicode strs

--- xonsh/xoreutils/uptime.py
+++ xonsh/xoreutils/uptime.py
@@ -100,13 +100,13 @@ def _uptime_bsd():
         return None
     # Determine how much space we need for the response.
     sz = ctypes.c_uint(0)
-    xp.LIBC.sysctlbyname("kern.boottime", None, ctypes.byref(sz), None, 0)
+    xp.LIBC.sysctlbyname(b"kern.boottime", None, ctypes.byref(sz), None, 0)
     if sz.value != struct.calcsize("@LL"):
         # Unexpected, let's give up.
         return None
     # For real now.
     buf = ctypes.create_string_buffer(sz.value)
-    xp.LIBC.sysctlbyname("kern.boottime", buf, ctypes.byref(sz), None, 0)
+    xp.LIBC.sysctlbyname(b"kern.boottime", buf, ctypes.byref(sz), None, 0)
     sec, usec = struct.unpack_from("@LL", buf.raw)
     # OS X disagrees what that second value is.
     if usec > 1000000:
