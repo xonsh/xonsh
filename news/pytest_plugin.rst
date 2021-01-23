4ae9b8461930e63609251659bf9ace8776203e00
Abdelrahman Elbehery <abdulrahman.elbehery95@gmail.com>
2021-01-23 17:34:49 +0200

    rework old pytest plugin

---
+++ news/pytest_plugin.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* More generic pytest plugin that allows utilizing fixtures, parametrization and more of pytest core features
+
+**Changed:**
+
+* Existing pytest plugin
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* <news item>
+
+**Security:**
+
+* <news item>
--- xonsh/pytest_plugin.py
+++ xonsh/pytest_plugin.py
@@ -1,78 +1,42 @@
 # -*- coding: utf-8 -*-
 """Pytest plugin for testing xsh files."""
-import sys
-import importlib
-from traceback import format_list, extract_tb
-
-import pytest
+import _pytest.pathlib
 
 from xonsh.main import setup
 
 
+class XshPytestPlugin:
+    def __init__(self):
+        setup()
+
+    def pytest_collect_file(self, path, parent):
+        if path.ext.lower() == ".xsh":
+            if not parent.session.isinitpath(path):
+                all_valid_test_file_patterns = parent.config.getini("python_files") + [
+                    "__init__.py"
+                ]
+                if not any(
+                    _pytest.pathlib.fnmatch_ex(pattern, path)
+                    for pattern in all_valid_test_file_patterns
+                ):
+                    return None
+            i_hook = parent.session.gethookproxy(path)
+            return i_hook.pytest_pycollect_makemodule(
+                fspath=path, path=path, parent=parent
+            )
+        return None
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("xonsh", "Collecting and running .xsh pytest files")
+    group.addoption(
+        "--xonsh",
+        default=False,
+        action="store_true",
+        help="Enables collecting and running xonsh test files ending with .xsh",
+    )
+
+
 def pytest_configure(config):
-    setup()
-
-
-def pytest_collection_modifyitems(items):
-    """Move xsh test first to work around a bug in normal
-    pytest cleanup. The order of tests are otherwise preserved.
-    """
-    xsh_items = []
-    other_items = []
-    for item in items:
-        if isinstance(item, XshFunction):
-            xsh_items.append(item)
-        else:
-            other_items.append(item)
-    items[:] = xsh_items + other_items
-
-
-def _limited_traceback(excinfo):
-    """Return a formatted traceback with all the stack
-    from this frame (i.e __file__) up removed
-    """
-    tb = extract_tb(excinfo.tb)
-    try:
-        idx = [__file__ in e for e in tb].index(True)
-        return format_list(tb[idx + 1 :])
-    except ValueError:
-        return format_list(tb)
-
-
-def pytest_collect_file(parent, path):
-    if path.ext.lower() == ".xsh" and path.basename.startswith("test_"):
-        return XshFile.from_parent(parent, fspath=path)
-
-
-class XshFile(pytest.File):
-    def collect(self):
-        sys.path.append(self.fspath.dirname)
-        mod = importlib.import_module(self.fspath.purebasename)
-        sys.path.pop(0)
-        tests = [t for t in dir(mod) if t.startswith("test_")]
-        for test_name in tests:
-            obj = getattr(mod, test_name)
-            if hasattr(obj, "__call__"):
-                yield XshFunction.from_parent(
-                    self, name=test_name, test_func=obj, test_module=mod
-                )
-
-
-class XshFunction(pytest.Item):
-    def __init__(self, name, parent, test_func, test_module):
-        super().__init__(name, parent)
-        self._test_func = test_func
-        self._test_module = test_module
-
-    def runtest(self, *args, **kwargs):
-        self._test_func(*args, **kwargs)
-
-    def repr_failure(self, excinfo):
-        """ called when self.runtest() raises an exception. """
-        formatted_tb = _limited_traceback(excinfo)
-        formatted_tb.insert(0, "xonsh execution failed\n")
-        formatted_tb.append("{}: {}".format(excinfo.type.__name__, excinfo.value))
-        return "".join(formatted_tb)
-
-    def reportinfo(self):
-        return self.fspath, 0, "xonsh test: {}".format(self.name)
+    if config.option.xonsh:
+        config.pluginmanager.register(XshPytestPlugin())
