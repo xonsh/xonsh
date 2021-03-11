2e1cbb65561f5666f8df35de017cbb946e95470b
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-11 18:48:40 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -202,7 +202,7 @@ html_favicon = "_static/magic_conch.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["_static", "_static/robots.txt"]
 html_style = "numpy_friendly.css"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
