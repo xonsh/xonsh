e7b0977910c0aba86a88160fe0406c0c2a069c5d
Andy Kipp <anki-code@users.noreply.github.com>
2021-04-12 22:31:45 +0300

    feat: Added environment variable to hide unwanted printing the stderr when using captured object. (#4221)
    * Added XONSH_SUBPROC_CAPTURED_PRINT_STDERR
    
    * black
    
    Co-authored-by: a <a@a.a>

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -174,6 +174,22 @@ print(x.returncode)
         "hallo on err\n1\n",
         0,
     ),
+    # test captured streaming alias without stderr
+    (
+        """
+def _test_stream(args, stdin, stdout, stderr):
+    print('hallo on err', file=stderr)
+    print('hallo on out', file=stdout)
+    return 1
+
+aliases['test-stream'] = _test_stream
+with __xonsh__.env.swap(XONSH_SUBPROC_CAPTURED_PRINT_STDERR=False):
+    x = !(test-stream)
+    print(x.returncode)
+""",
+        "1\n",
+        0,
+    ),
     # test piping aliases
     (
         """
