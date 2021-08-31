f0238026eeb55371fbc4b0c5af46b1de0ad22a37
Noortheen Raja <jnoortheen@gmail.com>
2021-08-31 11:57:56 +0530

    fix: failing tests because autovox is already loaded in some py versions

--- tests/completers/test_xompletions.py
+++ tests/completers/test_xompletions.py
@@ -39,7 +39,7 @@ def test_xonfig(args, prefix, exp, xsh_with_aliases, monkeypatch, check_complete
             {
                 # the list may vary wrt the env. so testing only part of the coreutils.
                 "abbrevs",
-                "autovox",
+                "pdb",
                 "bashisms",
                 "coreutils",
             },
