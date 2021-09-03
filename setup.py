973c4f89ced374625660962af6ce515305581a16
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2021-09-02 21:28:49 -0400

    Add keywords
    Copied from the repo's tags, minus "hacktoberfest"

--- setup.py
+++ setup.py
@@ -306,6 +306,7 @@ def main():
             "Topic :: System :: Shells",
             "Topic :: System :: System Shells",
         ],
+        keywords="python shell cli command-line prompt xonsh",
         packages=[
             "xonsh",
             "xonsh.ply.ply",
