e534285a2998dddd50dbae07b467b42c72e64ed1
Michael Ramsey <mike@hackerdise.me>
2021-09-06 21:08:46 -0400

    Update tools.py
    Removed commented out code left in originally

--- xonsh/tools.py
+++ xonsh/tools.py
@@ -285,7 +285,6 @@ class EnvPath(cabc.MutableSequence):
         elif replace:
             # https://stackoverflow.com/a/25251306/1621381
             self._l = list(filter(lambda x: x != data, self._l))
-            # self._l.remove(data)
             self._l.insert(0 if front else len(self._l), data)
 
 
