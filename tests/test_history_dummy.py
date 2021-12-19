a83ee8493f04a9a9186b1ac6383ca3b93ea618ef
Noortheen Raja <jnoortheen@gmail.com>
2021-12-19 10:29:20 +0530

    test: test refactor

--- tests/test_history_dummy.py
+++ tests/test_history_dummy.py
@@ -1,20 +1,12 @@
 """Tests the dummy history backend."""
-# pylint: disable=protected-access
+
+import pytest
 
 from xonsh.history.dummy import DummyHistory
 from xonsh.history.main import construct_history
 
 
-def test_construct_history_str(xession):
-    xession.env["XONSH_HISTORY_BACKEND"] = "dummy"
-    assert isinstance(construct_history(), DummyHistory)
-
-
-def test_construct_history_class(xession):
-    xession.env["XONSH_HISTORY_BACKEND"] = DummyHistory
-    assert isinstance(construct_history(), DummyHistory)
-
-
-def test_construct_history_instance(xession):
-    xession.env["XONSH_HISTORY_BACKEND"] = DummyHistory()
+@pytest.mark.parametrize("backend", ["dummy", DummyHistory, DummyHistory()])
+def test_construct_history_str(xession, backend):
+    xession.env["XONSH_HISTORY_BACKEND"] = backend
     assert isinstance(construct_history(), DummyHistory)
