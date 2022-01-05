c01fc6efe37bbd314779e62f80f9de598b9680bc
Noorhteen Raja NJ <jnoortheen@gmail.com>
2022-01-05 20:14:53 +0530

    refactor: do not override sys coreutils (#4617)
    * refactor: do not override sys coreutils
    
    * docs:

--- xontrib/coreutils.py
+++ xontrib/coreutils.py
@@ -13,13 +13,18 @@ posix command line utility with the same name. This is because these
 tools avoid the need for a full subprocess call. Additionally, these
 tools are cross-platform.
 """
+from xonsh.built_ins import XSH
+from xonsh.platform import (
+    ON_POSIX,
+)
 from xonsh.xoreutils.cat import cat
 from xonsh.xoreutils.echo import echo
 from xonsh.xoreutils.pwd import pwd
 from xonsh.xoreutils.tee import tee
 from xonsh.xoreutils.tty import tty
+from xonsh.xoreutils.umask import umask
+from xonsh.xoreutils.uptime import uptime
 from xonsh.xoreutils.yes import yes
-from xonsh.built_ins import XSH
 
 __all__ = ()
 
@@ -29,3 +34,10 @@ XSH.aliases["pwd"] = pwd
 XSH.aliases["tee"] = tee
 XSH.aliases["tty"] = tty
 XSH.aliases["yes"] = yes
+XSH.aliases["umask"] = umask
+XSH.aliases["uptime"] = uptime
+
+if ON_POSIX:
+    from xonsh.xoreutils.ulimit import ulimit
+
+    XSH.aliases["ulimit"] = ulimit
