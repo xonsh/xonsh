216bf58dc0bd4012a4388bdaf9484a9e9540c54d
Noortheen Raja <jnoortheen@gmail.com>
2022-01-16 17:37:38 +0530

    chore: watch top-level and nested files

--- docs/serve_docs.py
+++ docs/serve_docs.py
@@ -8,10 +8,11 @@ server = Server()
 if "no" not in sys.argv:
     exts = ("rst", "py", "jinja2")
     print(f"Watching file changes {exts}")
+    cmd = shell("make html", cwd=str(cur_dir))
     for ext in exts:
-        server.watch(
-            str(cur_dir / f"*.{ext}"),
-            shell("make html", cwd=str(cur_dir)),
-        )
+        # nested or
+        server.watch(str(cur_dir / f"**.{ext}"), cmd)
+        # top level
+        server.watch(str(cur_dir / f"**/*.{ext}"), cmd)
 
 server.serve(root=str(cur_dir / "_build" / "html"))
