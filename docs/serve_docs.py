6748752376ffc15ed38221c92824dd8d0b8b3f43
Noortheen Raja <jnoortheen@gmail.com>
2022-01-16 15:16:11 +0530

    style:

--- docs/serve_docs.py
+++ docs/serve_docs.py
@@ -6,7 +6,9 @@ cur_dir = Path(__file__).parent
 server = Server()
 
 if "no" not in sys.argv:
-    for ext in ("rst", "py", "jinja2"):
+    exts = ("rst", "py", "jinja2")
+    print(f"Watching file changes {exts}")
+    for ext in exts:
         server.watch(
             str(cur_dir / f"*.{ext}"),
             shell("make html", cwd=str(cur_dir)),
