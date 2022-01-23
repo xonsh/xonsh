c4294ea44bf0e1102911f1ecc4b918bbf8fc8ab6
Noortheen Raja <jnoortheen@gmail.com>
2022-01-23 19:18:28 +0530

    todo: improve man page completions

--- tests/test_man.py
+++ tests/test_man.py
@@ -1,27 +1,22 @@
 import os
+
 import pytest  # noqa F401
-from xonsh.completers.man import complete_from_man
 
 from tools import skip_if_on_windows
-
-from xonsh.parsers.completion_context import (
-    CompletionContext,
-    CommandContext,
-    CommandArg,
-)
+from xonsh.completers.man import complete_from_man
 
 
 @skip_if_on_windows
-def test_man_completion(monkeypatch, tmpdir, xession):
-    tempdir = tmpdir.mkdir("test_man")
-    monkeypatch.setitem(
-        os.environ, "MANPATH", os.path.dirname(os.path.abspath(__file__))
-    )
-    xession.env.update({"XONSH_DATA_DIR": str(tempdir)})
-    completions = complete_from_man(
-        CompletionContext(
-            CommandContext(args=(CommandArg("yes"),), arg_index=1, prefix="--")
-        )
-    )
-    assert "--version" in completions
-    assert "--help" in completions
+@pytest.mark.parametrize(
+    "cmd,exp,set_manpath",
+    [
+        ["yes --", {"--version", "--help"}, True],
+        ["man --", {"--help"}, False],
+    ],
+)
+def test_man_completion(tmpdir, xession, completer_obj, cmd, exp, set_manpath):
+    if set_manpath:
+        xession.env["MANPATH"] = os.path.dirname(os.path.abspath(__file__))
+    ctx = completer_obj.parse(cmd)
+    completions = set(map(str, complete_from_man(ctx)))
+    assert completions == exp
--- xonsh/completers/man.py
+++ xonsh/completers/man.py
@@ -1,17 +1,23 @@
+import functools
+import json
 import os
 import re
 import pickle
 import subprocess
+import textwrap
 import typing as tp
+from pathlib import Path
 
 from xonsh.parsers.completion_context import CommandContext
 from xonsh.built_ins import XSH
 import xonsh.lazyasd as xl
 
-from xonsh.completers.tools import get_filter_function, contextual_command_completer
-
-OPTIONS: tp.Optional[tp.Dict[str, tp.Any]] = None
-OPTIONS_PATH: tp.Optional[str] = None
+from xonsh.completers.tools import (
+    get_filter_function,
+    contextual_command_completer,
+    sub_proc_get_output,
+    RichCompletion,
+)
 
 
 @xl.lazyobject
@@ -24,37 +30,105 @@ def INNER_OPTIONS_RE():
     return re.compile(r"-\w|--[a-z0-9-]+")
 
 
+@functools.lru_cache
+def get_man_completions_path() -> Path:
+    env = XSH.env or {}
+    datadir = Path(env["XONSH_DATA_DIR"]) / "man_completions_cache"
+    if not datadir.exists():
+        datadir.mkdir(exist_ok=True, parents=True)
+    return datadir
+
+
+def _get_man_page(cmd: str):
+    """without control characters"""
+    env = XSH.env.detype()
+    manpage = subprocess.Popen(
+        ["man", cmd], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, env=env
+    )
+    # This is a trick to get rid of reverse line feeds
+    return subprocess.check_output(["col", "-b"], stdin=manpage.stdout, env=env)
+
+
+def generate_options_of(cmd: str):
+    out = _get_man_page(cmd)
+    if not out:
+        return
+
+    def get_headers(text: str):
+        """split as header-body based on indent"""
+        header = ""
+        body = []
+        for line in textwrap.dedent(text).splitlines():
+            if not line.strip():
+                continue
+            if line.startswith(" "):
+                body.append(line)
+            else:
+                if header or body:
+                    yield header, body
+
+                # found new section
+                header = line.strip().lower()
+                body = []
+        if header or body:
+            yield header, body
+
+    def get_opt_headers(text: str):
+        for header, lines in get_headers(text):
+            if header in {
+                "description",
+                "options",
+                "command options",
+            }:
+                yield header, "\n".join(lines)
+
+    def split_options_string(text: str):
+        done_options = False
+        options = []
+        rest = []
+        for part in text.split():
+            if (not done_options) and part.startswith("-"):
+                options.append(part.rstrip(","))
+            else:
+                done_options = True
+                rest.append(part)
+        return options, " ".join(rest)
+
+    for header, body in get_opt_headers(out.decode()):
+        # return old section if
+        for opt, lines in get_headers(body):
+            if opt.startswith("-"):
+                # sometime a single line will have both desc and options
+                option_strings, rest = split_options_string(opt)
+                descs = []
+                if rest:
+                    descs.append(rest)
+                if lines:
+                    descs.append(textwrap.dedent("\n".join(lines)))
+
+                yield "\n".join(descs), tuple(option_strings)
+
+
+@functools.lru_cache(maxsize=10)
+def get_options_of(cmd: str) -> "dict[str, tuple[str, ...]]":
+    path = get_man_completions_path() / f"{cmd}.json"
+    if path.exists():
+        return json.loads(path.read_text())
+    options = dict(generate_options_of(cmd))
+    path.write_text(json.dumps(options))
+    return options
+
+
 @contextual_command_completer
 def complete_from_man(context: CommandContext):
     """
     Completes an option name, based on the contents of the associated man
     page.
     """
-    global OPTIONS, OPTIONS_PATH
-    if OPTIONS is None:
-        datadir: str = XSH.env["XONSH_DATA_DIR"]  # type: ignore
-        OPTIONS_PATH = os.path.join(datadir, "man_completions_cache")
-        try:
-            with open(OPTIONS_PATH, "rb") as f:
-                OPTIONS = pickle.load(f)
-        except Exception:
-            OPTIONS = {}
+
     if context.arg_index == 0 or not context.prefix.startswith("-"):
-        return set()
+        return
     cmd = context.args[0].value
-    if cmd not in OPTIONS:
-        try:
-            manpage = subprocess.Popen(
-                ["man", cmd], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
-            )
-            # This is a trick to get rid of reverse line feeds
-            enc_text = subprocess.check_output(["col", "-b"], stdin=manpage.stdout)
-            text = enc_text.decode("utf-8")
-            scraped_text = " ".join(SCRAPE_RE.findall(text))
-            matches = INNER_OPTIONS_RE.findall(scraped_text)
-            OPTIONS[cmd] = matches
-            with open(tp.cast(str, OPTIONS_PATH), "wb") as f:
-                pickle.dump(OPTIONS, f)
-        except Exception:
-            return set()
-    return {s for s in OPTIONS[cmd] if get_filter_function()(s, context.prefix)}
+
+    for desc, opts in get_options_of(cmd).items():
+        yield RichCompletion(value=opts[-1], display=", ".join(opts), description=desc)
