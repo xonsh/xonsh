6e7ad5096864cd936f2c59bad0eb0b4d89e28f22
Noortheen Raja <jnoortheen@gmail.com>
2022-03-24 00:52:02 +0530

    test: update tests

--- tests/aliases/test_source.py
+++ tests/aliases/test_source.py
@@ -38,8 +38,8 @@ def test_source_path(mockopen, mocked_execx_checker, patch_locate_binary, xessio
     patch_locate_binary(xession.commands_cache)
 
     source_alias(["foo", "bar"])
-    path_foo = os.path.join("tests", "bin", "foo")
-    path_bar = os.path.join("tests", "bin", "bar")
+    path_foo = os.path.join("bin", "foo")
+    path_bar = os.path.join("bin", "bar")
     assert mocked_execx_checker[0].endswith(path_foo)
     assert mocked_execx_checker[1].endswith(path_bar)
 
