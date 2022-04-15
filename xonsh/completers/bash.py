5292910e1478405bf01f0cb2adcd638b7fed6451
Peter Ye <yaxollum@gmail.com>
2022-04-15 10:37:18 -0400

    Apply trailing equal sign fix to _bash_quote_paths()
    Use _bash_quote_paths instead of _quote_paths in complete_from_bash()

--- xonsh/completers/bash.py
+++ xonsh/completers/bash.py
@@ -3,8 +3,7 @@
 import xonsh.platform as xp
 import xonsh.tools as xt
 from xonsh.built_ins import XSH
-from xonsh.completers.bash_completion import bash_completions
-from xonsh.completers.path import _quote_paths
+from xonsh.completers.bash_completion import _bash_quote_paths, bash_completions
 from xonsh.completers.tools import RichCompletion, contextual_command_completer
 from xonsh.parsers.completion_context import CommandContext
 
@@ -41,7 +40,7 @@ def complete_from_bash(context: CommandContext):
         env=env,
         paths=paths,
         command=command,
-        quote_paths=_quote_paths,
+        quote_paths=_bash_quote_paths,
         line_args=args,
         opening_quote=opening_quote,
         closing_quote=closing_quote,
