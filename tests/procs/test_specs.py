09f3a4ea143907f889d5b40b4143caf7363000aa
Peter Ye <yaxollum@gmail.com>
2022-08-10 22:47:24 -0400

    Fix `cd` command error message when `$THREAD_SUBPROCS=False`

--- tests/procs/test_specs.py
+++ tests/procs/test_specs.py
@@ -166,3 +166,17 @@ def test_callable_alias_cls(thread_subprocs, xession):
     spec = cmds_to_specs(cmds, captured="stdout")[0]
     proc = spec.run()
     assert proc.f == obj
+
+
+def test_procproxy_not_captured(xession):
+    xession.aliases["tst"] = lambda: 0
+    cmds = (["tst", "/root"],)
+
+    xession.env["THREAD_SUBPROCS"] = False
+    specs = cmds_to_specs(cmds, captured="hiddenobject")
+
+    assert specs[0].cls is ProcProxy
+
+    # neither stdout nor stderr should be captured
+    assert specs[0].stdout is None
+    assert specs[0].stderr is None
