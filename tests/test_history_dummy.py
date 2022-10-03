7271647c50043e8bb0c9c302398795f73db4fef4
Blake Ramsdell <blaker@gmail.com>
2022-10-03 10:43:04 -0700

    Add broken test for XONSH_IGNORE_REGEX

--- tests/test_history_dummy.py
+++ tests/test_history_dummy.py
@@ -10,3 +10,19 @@ from xonsh.history.main import construct_history
 def test_construct_history_str(xession, backend):
     xession.env["XONSH_HISTORY_BACKEND"] = backend
     assert isinstance(construct_history(), DummyHistory)
+
+
+@pytest.mark.parametrize("backend", ["dummy", DummyHistory, DummyHistory()])
+def test_ignore_regex_invalid(xession, backend):
+    xession.env["XONSH_HISTORY_BACKEND"] = backend
+    xession.env["XONSH_IGNORE_REGEX"] = "**"
+    history = construct_history()
+    assert history.history_ignore_regex is None
+
+
+@pytest.mark.parametrize("backend", ["dummy", DummyHistory, DummyHistory()])
+def test_ignore_regex_valid(xession, backend):
+    xession.env["XONSH_HISTORY_BACKEND"] = backend
+    xession.env["XONSH_IGNORE_REGEX"] = "ls"
+    history = construct_history()
+    assert history.history_ignore_regex is not None
