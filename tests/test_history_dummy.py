0b04f404f22819015bf01ba03c6dc3b105fa7e3e
Blake Ramsdell <blaker@gmail.com>
2022-10-03 11:17:37 -0700

    Remove extra test variants

--- tests/test_history_dummy.py
+++ tests/test_history_dummy.py
@@ -12,17 +12,17 @@ def test_construct_history_str(xession, backend):
     assert isinstance(construct_history(), DummyHistory)
 
 
-@pytest.mark.parametrize("backend", ["dummy", DummyHistory, DummyHistory()])
-def test_ignore_regex_invalid(xession, backend):
-    xession.env["XONSH_HISTORY_BACKEND"] = backend
+def test_ignore_regex_invalid(xession):
+    xession.env["XONSH_HISTORY_BACKEND"] = "dummy"
     xession.env["XONSH_IGNORE_REGEX"] = "**"
     history = construct_history()
+    assert isinstance(history, DummyHistory)
     assert history.history_ignore_regex is None
 
 
-@pytest.mark.parametrize("backend", ["dummy", DummyHistory, DummyHistory()])
-def test_ignore_regex_valid(xession, backend):
-    xession.env["XONSH_HISTORY_BACKEND"] = backend
+def test_ignore_regex_valid(xession):
+    xession.env["XONSH_HISTORY_BACKEND"] = "dummy"
     xession.env["XONSH_IGNORE_REGEX"] = "ls"
     history = construct_history()
+    assert isinstance(history, DummyHistory)
     assert history.history_ignore_regex is not None
