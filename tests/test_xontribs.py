5daca8e0129e3bf278d35e0b0b480b0abf49e222
a <1@1.1>
2023-02-16 11:55:16 +0600

    Transfer abbrevs to xontrib-abbrevs

--- tests/test_xontribs.py
+++ tests/test_xontribs.py
@@ -161,4 +161,4 @@ hello = 'world'
 def test_xontrib_list(xession, capsys):
     xontribs_main(["list"])
     out, err = capsys.readouterr()
-    assert "abbrevs" in out
+    assert "hello_world" in out
