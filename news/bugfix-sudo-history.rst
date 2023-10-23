c3085399f291e1168838e0a69be459f074bf5bd3
doronz <doron88@gmail.com>
2023-10-23 20:45:55 +0300

    history: fix json file permissions upon `sudo` (#5220)

---
+++ news/bugfix-sudo-history.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* history: fix json file permissions upon `sudo` (#5220)
+
+**Security:**
+
+* <news item>
--- xonsh/history/json.py
+++ xonsh/history/json.py
@@ -426,6 +426,10 @@ class JsonHistory(History):
                 xlj.ljdump(meta, f, sort_keys=True)
 
             try:
+                sudo_uid = os.environ.get("SUDO_UID")
+                sudo_gid = os.environ.get("SUDO_GID")
+                if None not in (sudo_uid, sudo_gid):
+                    os.chown(self.filename, int(sudo_uid), int(sudo_gid))
                 os.chmod(self.filename, 0o600)
             except Exception:  # pylint: disable=broad-except
                 pass
