53782e4d03af5e1901722640da017e171921723b
a <1@1.1>
2024-05-08 12:41:27 +0200

    wip

--- xonsh/main.py
+++ xonsh/main.py
@@ -584,7 +584,8 @@ def main_xonsh(args):
             err_type, err, _ = exc_info
             if err_type is SystemExit:
                 XSH.exit = True
-                exit_code = int(exc_info[1].code)
+                code = getattr(exc_info[1], 'code', 0)
+                exit_code = int(code) if code is not None else 0
             else:
                 exit_code = 1
                 print_exception(None, exc_info)
