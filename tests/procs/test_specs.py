c2f05196c30218539f20405eadf89464224b3218
a <1@1.1>
2024-05-14 18:25:34 +0200

    Tests

--- tests/procs/test_specs.py
+++ tests/procs/test_specs.py
@@ -191,8 +191,6 @@ def test_suspended_captured_process_pipeline(xonsh_session):
     assert len(jobs) == 3
     for j in jobs:
         assert j["status"] == "suspended"
-        for pid in j["pids"]:
-            os.kill(pid, signal.SIGKILL)
 
 
 @skip_if_on_windows
