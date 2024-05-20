76ed95dc56c1779d55dea21c345af95404c895da
a <1@1.1>
2024-05-20 16:54:37 +0200

    Set message

--- xonsh/history/main.py
+++ xonsh/history/main.py
@@ -40,7 +40,9 @@ def construct_history(backend=None, **kwargs) -> "History":
     try:
         return kls_history(**kwargs)
     except Exception as e:
-        xt.print_exception(f"Error during load {kls_history}: {e}\nHistory disabled.")
+        xt.print_exception(f"Error during load {kls_history}: {e}\n"
+                           f"Set $XONSH_HISTORY_BACKEND='dummy' to disable history.\n"
+                           f"History disabled.")
         return DummyHistory()
 
 
