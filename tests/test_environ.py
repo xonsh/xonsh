bd1ed3d36c6128c9994e11942e511c3b6a6ac6cd
a <1@1.1>
2024-05-21 19:03:48 +0200

    tests

--- tests/test_environ.py
+++ tests/test_environ.py
@@ -77,10 +77,11 @@ def test_env_detype_no_dict():
 
 def test_env_detype_all():
     env = Env()
+    env._vars['DEFAULT'] = Var.with_default(1)
     env._detyped, det = None, env.detype()
     env._detyped, det_all = None, env.detype_all()
-    assert "PROMPT" not in det
-    assert "PROMPT" in det_all
+    assert "DEFAULT" not in det
+    assert "DEFAULT" in det_all
 
 
 def test_histcontrol_none():
