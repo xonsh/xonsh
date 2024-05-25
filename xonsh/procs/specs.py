5b52a0aae05fdac3ff7cb7a71870d8350aa2c734
a <1@1.1>
2024-05-25 07:22:00 +0200

    Revert jobs.obj rename.

--- xonsh/procs/specs.py
+++ xonsh/procs/specs.py
@@ -956,7 +956,7 @@ def _run_command_pipeline(specs, cmds):
                 "cmds": cmds,
                 "pids": [i.pid for i in cp.procs],
                 "status": "suspended" if cp.suspended else "running",
-                "proc": proc,
+                "obj": proc,
                 "bg": background,
                 "pipeline": cp,
                 "pgrp": cp.term_pgid,
