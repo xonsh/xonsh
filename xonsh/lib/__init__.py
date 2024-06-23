890b8748d2c7a6478098c6c877cdcd6de280ede2
a <1@1.1>
2024-06-23 03:16:28 +0200

    doc

--- xonsh/lib/__init__.py
+++ xonsh/lib/__init__.py
@@ -3,7 +3,7 @@ Originally posted by @scopatz in https://github.com/xonsh/xonsh/issues/2726#issu
 
     Recently @CJ-Wright has started up a ``xonsh.lib`` sub-package, which is usable from pure Python.
     This is meant as a standard library for xonsh and downstream tools.
-    Currently there are some xonsh-ish wrappers around `os` and `subprocess`.
+    Currently there are some xonsh-ish wrappers around ``os`` and ``subprocess``.
     We'd love to see more contributions to this effort! So if there is something
     like ``sh()`` that you'd like to see, by all means please help us add it!
 """
