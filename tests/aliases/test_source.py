20eb825b77a6a35eda4a87c1af14e3e5240908af
a <1@1.1>
2024-06-27 16:52:02 +0200

    Fix source test

--- tests/aliases/test_source.py
+++ tests/aliases/test_source.py
@@ -2,7 +2,7 @@ import builtins
 import os.path
 from contextlib import contextmanager
 from unittest.mock import MagicMock
-
+from pathlib import Path
 import pytest
 
 from xonsh.aliases import make_default_aliases, source_alias
@@ -34,10 +34,9 @@ def test_source_current_dir(mockopen, monkeypatch, mocked_execx_checker):
     assert mocked_execx_checker == ["foo", "bar"]
 
 
-def test_source_path(mockopen, mocked_execx_checker, patch_locate_binary, xession):
-    patch_locate_binary(xession.commands_cache)
-
-    source_alias(["foo", "bar"])
+def test_source_path(mockopen, mocked_execx_checker, xession):
+    with xession.env.swap(PATH=Path(__file__).parent.parent / 'bin'):
+        source_alias(["foo", "bar"])
     path_foo = os.path.join("bin", "foo")
     path_bar = os.path.join("bin", "bar")
     assert mocked_execx_checker[0].endswith(path_foo)
