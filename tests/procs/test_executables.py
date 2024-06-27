beaa2e331a31017ed1886e77f4a8ae6361b79cb2
a <1@1.1>
2024-06-27 15:57:13 +0200

    tests

--- tests/procs/test_executables.py
+++ tests/procs/test_executables.py
@@ -2,11 +2,11 @@ import os
 
 from xonsh.platform import ON_WINDOWS
 from xonsh.procs.executables import get_possible_names, locate_executable
-
+from xonsh.environ import Env
 
 def test_get_possible_names(xession):
-    xession.env["PATHEXT"] = [".EXE", ".COM"]
-    assert get_possible_names("file") == ["file", "file.EXE", "file.COM"]
+    env = Env(PATHEXT=[".EXE", ".COM"])
+    assert get_possible_names("file", env) == ["file", "file.EXE", "file.COM"]
 
 
 def test_xonshrc(tmpdir, xession):
@@ -20,17 +20,18 @@ def test_xonshrc(tmpdir, xession):
         if exefile in executables:
             os.chmod(f, 0o777)
 
-    xession.env["PATH"].append(bindir)
-    xession.env["PATHEXT"] = [".EXE", ".COM"]
+    env = Env(PATH=str(bindir), PATHEXT=[".EXE", ".COM"])
 
-    assert locate_executable("runme")
-    assert locate_executable("cmd")
-    assert locate_executable("cmd.EXE")
-    assert locate_executable("cmdcom").endswith("cmdcom.EXE")
-    assert locate_executable("nofile") is None
-    assert locate_executable("nonono") is None
-    assert locate_executable("subdir") is None
+    assert locate_executable("runme", env)
+    assert locate_executable("cmd.EXE", env)
+    assert locate_executable("nofile", env) is None
+    assert locate_executable("nonono", env) is None
+    assert locate_executable("subdir", env) is None
     if ON_WINDOWS:
-        assert locate_executable("no")
+        assert locate_executable("cmd", env)
+        assert locate_executable("no", env)
+        assert locate_executable("cmdcom", env).endswith("cmdcom.EXE")
     else:
-        assert locate_executable("no") is None
+        assert locate_executable("cmd", env) is None
+        assert locate_executable("no", env) is None
+        assert locate_executable("cmdcom", env) is None
