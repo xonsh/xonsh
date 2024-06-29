9c2f18838ba44261ddd1ea75e0decd86a662ad1e
a <1@1.1>
2024-06-29 10:58:44 +0200

    doc

--- xonsh/api/subprocess.py
+++ xonsh/api/subprocess.py
@@ -1,13 +1,5 @@
 """Xonsh extension of the standard library subprocess module, using xonsh for
-subprocess calls
-
-Originally posted by @scopatz in https://github.com/xonsh/xonsh/issues/2726#issuecomment-406447196 :
-    Recently @CJ-Wright has started up a ``xonsh.lib`` sub-package, which is usable from pure Python.
-    This is meant as a standard library for xonsh and downstream tools.
-    Currently there are some xonsh-ish wrappers around ``os`` and ``subprocess``.
-    We'd love to see more contributions to this effort! So if there is something
-    like ``sh()`` that you'd like to see, by all means please help us add it!
-"""
+subprocess calls"""
 
 from xonsh.api.os import indir
 from xonsh.built_ins import XSH, subproc_captured_hiddenobject, subproc_captured_stdout
