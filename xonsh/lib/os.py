4745fab2c34b2976ccecde94288f9522f60536fa
a <1@1.1>
2024-06-29 10:26:15 +0200

    comment

--- xonsh/lib/os.py
+++ xonsh/lib/os.py
@@ -1,5 +1,14 @@
 """Xonsh extension of the standard library os module, using xonsh for
-subprocess calls"""
+subprocess calls
+
+Originally posted in https://github.com/xonsh/xonsh/issues/2726#issuecomment-406447196 :
+    Recently @CJ-Wright has started up a ``xonsh.lib`` sub-package, which is usable from pure Python.
+    This is meant as a standard library for xonsh and downstream tools.
+    Currently there are some xonsh-ish wrappers around ``os`` and ``subprocess``.
+    We'd love to see more contributions to this effort! So if there is something
+    like ``sh()`` that you'd like to see, by all means please help us add it!
+"""
+
 
 import sys
 
