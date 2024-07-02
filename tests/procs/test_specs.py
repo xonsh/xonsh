xonsh/xonsh

commit 7ce53f1844739160dbb4e82d86305e27a607a2b6

Always capture aliases (#4445)
* specs: Make sure sub-specs are always captured regardless of $XONSH_CAPTURE_ALWAYS

* procs: Ensure env is used in functional aliases

* news: Add always-capture-aliases

* tests: procs: Skip known issues

* tests: Restore all builtins in the 'xonsh_builtins' fixture

* tests: vc: Use monkeypatch to mock CommandsCache functions

* tests: main: Don't use the local machine's xonshrc

xonsh/procs/specs.py
@@ -449,11 +449,12 @@ class SubprocSpec:
         event_name = self._cmd_event_name()
         self._pre_run_event_fire(event_name)
         kwargs = {n: getattr(self, n) for n in self.kwnames}
-        self.prep_env(kwargs)
         if callable(self.alias):
+            kwargs["env"] = self.env or {}
             kwargs["env"]["__ALIAS_NAME"] = self.alias_name or ""
             p = self.cls(self.alias, self.cmd, **kwargs)
         else:
+            self.prep_env_subproc(kwargs)
             self.prep_preexec_fn(kwargs, pipeline_group=pipeline_group)
             self._fix_null_cmd_bytes()
             p = self._run_binary(kwargs)
@@ -493,7 +494,7 @@ class SubprocSpec:
             raise xt.XonshError(e)
         return p
 
-    def prep_env(self, kwargs):
+    def prep_env_subproc(self, kwargs):
         """Prepares the environment to use in the subprocess."""
         with XSH.env.swap(self.env) as env:
             denv = env.detype()
@@ -845,7 +846,18 @@ def cmds_to_specs(cmds, captured=False, envs=None):
             specs[i].background = True
         else:
             raise xt.XonshError(f"unrecognized redirect {redirect!r}")
+
     # Apply boundary conditions
+    if not XSH.env.get("XONSH_CAPTURE_ALWAYS"):
+        # Make sure sub-specs are always captured.
+        # I.e. ![some_alias | grep x] $(some_alias)
+        specs_to_capture = specs if captured in STDOUT_CAPTURE_KINDS else specs[:-1]
+        for spec in specs_to_capture:
+            if spec.env is None:
+                spec.env = {"XONSH_CAPTURE_ALWAYS": True}
+            else:
+                spec.env.setdefault("XONSH_CAPTURE_ALWAYS", True)
+
     _update_last_spec(specs[-1])
     return specs
 

tests/procs/test_specs.py
@@ -1,5 +1,6 @@
 """Tests the xonsh.procs.specs"""
 import itertools
+import sys
 from subprocess import Popen
 
 import pytest
@@ -7,7 +8,7 @@ import pytest
 from xonsh.procs.specs import cmds_to_specs, run_subproc
 from xonsh.built_ins import XSH
 from xonsh.procs.posix import PopenThread
-from xonsh.procs.proxies import ProcProxy, ProcProxyThread
+from xonsh.procs.proxies import ProcProxy, ProcProxyThread, STDOUT_DISPATCHER
 
 from .tools import skip_if_on_windows
 
@@ -60,13 +61,43 @@ def test_cmds_to_specs_capture_stdout_not_stderr(thread_subprocs):
 
 
 @skip_if_on_windows
+@pytest.mark.parametrize("pipe", (True, False))
+@pytest.mark.parametrize("alias_type", (None, "func", "exec", "simple"))
 @pytest.mark.parametrize(
     "thread_subprocs, capture_always", list(itertools.product((True, False), repeat=2))
 )
-def test_capture_always(capfd, thread_subprocs, capture_always):
+def test_capture_always(
+    capfd, thread_subprocs, capture_always, alias_type, pipe, monkeypatch
+):
+    if not thread_subprocs and alias_type in ["func", "exec"]:
+        if pipe:
+            return pytest.skip("https://github.com/xonsh/xonsh/issues/4443")
+        else:
+            return pytest.skip("https://github.com/xonsh/xonsh/issues/4444")
+
     env = XSH.env
-    exp = "HELLO"
+    exp = "HELLO\nBYE\n"
     cmds = [["echo", "-n", exp]]
+    if pipe:
+        exp = exp.splitlines()[1] + "\n"  # second line
+        cmds += ["|", ["grep", "--color=never", exp.strip()]]
+
+    if alias_type:
+        first_cmd = cmds[0]
+        # Enable capfd for function aliases:
+        monkeypatch.setattr(STDOUT_DISPATCHER, "default", sys.stdout)
+        if alias_type == "func":
+            XSH.aliases["tst"] = (
+                lambda: run_subproc([first_cmd], "hiddenobject") and None
+            )  # Don't return a value
+        elif alias_type == "exec":
+            first_cmd = " ".join(repr(arg) for arg in first_cmd)
+            XSH.aliases["tst"] = f"![{first_cmd}]"
+        else:
+            # alias_type == "simple"
+            XSH.aliases["tst"] = first_cmd
+
+        cmds[0] = ["tst"]
 
     env["THREAD_SUBPROCS"] = thread_subprocs
     env["XONSH_CAPTURE_ALWAYS"] = capture_always
@@ -86,7 +117,7 @@ def test_capture_always(capfd, thread_subprocs, capture_always):
     hidden = run_subproc(cmds, "object")  # !()
     hidden.end()
     if thread_subprocs:
-        assert not capfd.readouterr().out
+        assert exp not in capfd.readouterr().out
         assert hidden.out == exp
     else:
         # for some reason THREAD_SUBPROCS=False fails to capture in `!()` but still succeeds in `$()`
@@ -94,7 +125,7 @@ def test_capture_always(capfd, thread_subprocs, capture_always):
         assert not hidden.out
 
     output = run_subproc(cmds, "stdout")  # $()
-    assert not capfd.readouterr().out
+    assert exp not in capfd.readouterr().out
     assert output == exp
 
     # Explicitly non-captured commands are never captured (/always printed)

tests/conftest.py
@@ -88,7 +88,7 @@ def session_vars():
 @pytest.fixture
 def xonsh_builtins(monkeypatch, xonsh_events, session_vars):
     """Mock out most of the builtins xonsh attributes."""
-    old_builtins = set(dir(builtins))
+    old_builtins = dict(vars(builtins).items())  # type: ignore
 
     XSH.load(ctx={}, **session_vars)
     if ON_WINDOWS:
@@ -130,9 +130,12 @@ def xonsh_builtins(monkeypatch, xonsh_events, session_vars):
     # todo: remove using builtins for tests at all
     yield builtins
     XSH.unload()
-    for attr in set(dir(builtins)) - old_builtins:
+    for attr in set(dir(builtins)) - set(old_builtins):
         if hasattr(builtins, attr):
             delattr(builtins, attr)
+    for attr, old_value in old_builtins.items():
+        setattr(builtins, attr, old_value)
+
     tasks.clear()  # must to this to enable resetting all_jobs
 
 

tests/prompt/test_vc.py
@@ -91,22 +91,23 @@ current = yellow reverse
         assert not branch.startswith(u"\u001b[")
 
 
-def test_current_branch_calls_locate_binary_for_empty_cmds_cache(xession):
+def test_current_branch_calls_locate_binary_for_empty_cmds_cache(xession, monkeypatch):
     cache = xession.commands_cache
-    cache.is_empty = Mock(return_value=True)
-    cache.locate_binary = Mock(return_value="")
+    monkeypatch.setattr(cache, "is_empty", Mock(return_value=True))
+    monkeypatch.setattr(cache, "locate_binary", Mock(return_value=""))
     vc.current_branch()
     assert cache.locate_binary.called
 
 
 def test_current_branch_does_not_call_locate_binary_for_non_empty_cmds_cache(
     xession,
+    monkeypatch,
 ):
     cache = xession.commands_cache
-    cache.is_empty = Mock(return_value=False)
-    cache.locate_binary = Mock(return_value="")
+    monkeypatch.setattr(cache, "is_empty", Mock(return_value=False))
+    monkeypatch.setattr(cache, "locate_binary", Mock(return_value=""))
     # make lazy locate return nothing to avoid running vc binaries
-    cache.lazy_locate_binary = Mock(return_value="")
+    monkeypatch.setattr(cache, "lazy_locate_binary", Mock(return_value=""))
     vc.current_branch()
     assert not cache.locate_binary.called
 

tests/test_main.py
@@ -27,6 +27,13 @@ def shell(xession, monkeypatch):
     monkeypatch.setattr(xonsh.main, "Shell", Shell)
 
 
+@pytest.fixture(autouse=True)
+def empty_xonshrc(monkeypatch):
+    # Don't use the local machine's xonshrc
+    empty_file_path = "NUL" if ON_WINDOWS else "/dev/null"
+    monkeypatch.setitem(os.environ, "XONSHRC", empty_file_path)
+
+
 def test_premain_no_arg(shell, monkeypatch, xession):
     monkeypatch.setattr(sys.stdin, "isatty", lambda: True)
     xonsh.main.premain([])

xonsh/procs/proxies.py
@@ -414,7 +414,7 @@ class ProcProxyThread(threading.Thread):
         self.stdout = stdout
         self.stderr = stderr
         self.close_fds = close_fds
-        self.env = env or XSH.env
+        self.env = env
         self._interrupted = False
 
         if xp.ON_WINDOWS:
@@ -510,7 +510,7 @@ class ProcProxyThread(threading.Thread):
         # run the function itself
         try:
             alias_stack = XSH.env.get("__ALIAS_STACK", "")
-            if self.env.get("__ALIAS_NAME"):
+            if self.env and self.env.get("__ALIAS_NAME"):
                 alias_stack += ":" + self.env["__ALIAS_NAME"]
 
             with STDOUT_DISPATCHER.register(sp_stdout), STDERR_DISPATCHER.register(
@@ -518,7 +518,7 @@ class ProcProxyThread(threading.Thread):
             ), xt.redirect_stdout(STDOUT_DISPATCHER), xt.redirect_stderr(
                 STDERR_DISPATCHER
             ), XSH.env.swap(
-                __ALIAS_STACK=alias_stack
+                self.env, __ALIAS_STACK=alias_stack
             ):
                 r = self.f(self.args, sp_stdin, sp_stdout, sp_stderr, spec, spec.stack)
         except SystemExit as e:
@@ -817,7 +817,8 @@ class ProcProxy:
         stderr = self._pick_buf(self.stderr, sys.stderr, enc, err)
         # run the actual function
         try:
-            r = self.f(self.args, stdin, stdout, stderr, spec, spec.stack)
+            with XSH.env.swap(self.env):
+                r = self.f(self.args, stdin, stdout, stderr, spec, spec.stack)
         except Exception:
             xt.print_exception()
             r = 1

commit 61a77ac3cef60565d5056f50b687edab7fa4531f

Callable alias: fixed capturing stdout in case of redirect (#5527)
### Motivation

Closes #5512. 
The issue was introduced in #4445. It's needed to add checking the
redirect case.

### Before

```xsh
cd /tmp
@aliases.register('a')
def _a():
    print("1-hello")
    echo 2-hello
a > o.txt
# 2-hello
cat o.txt
# 1-hello
```

### After

```xsh
cd /tmp
@aliases.register('a')
def _a():
    print("1-hello")
    echo 2-hello
a > o.txt
cat o.txt
# 1-hello
# 2-hello
```

## For community
⬇️ **Please click the 👍 reaction instead of leaving a `+1` or 👍
comment**

---------

Co-authored-by: a <1@1.1>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

xonsh/procs/specs.py
@@ -1021,17 +1021,28 @@ def cmds_to_specs(cmds, captured=False, envs=None):
 
     # Apply boundary conditions
     if not XSH.env.get("XONSH_CAPTURE_ALWAYS"):
-        # Make sure sub-specs are always captured.
-        # I.e. ![some_alias | grep x] $(some_alias)
-        specs_to_capture = specs if captured in STDOUT_CAPTURE_KINDS else specs[:-1]
-        for spec in specs_to_capture:
-            if spec.env is None:
-                spec.env = {"XONSH_CAPTURE_ALWAYS": True}
-            else:
-                spec.env.setdefault("XONSH_CAPTURE_ALWAYS", True)
+        # Make sure sub-specs are always captured in case:
+        # `![some_alias | grep x]`, `$(some_alias)`, `some_alias > file`.
+        last = spec
+        is_redirected_stdout = bool(last.stdout)
+        specs_to_capture = (
+            specs
+            if captured in STDOUT_CAPTURE_KINDS or is_redirected_stdout
+            else specs[:-1]
+        )
+        _set_specs_capture_always(specs_to_capture)
 
     _update_last_spec(specs[-1])
     return specs
+
+
+def _set_specs_capture_always(specs_to_capture):
+    """Set XONSH_CAPTURE_ALWAYS for all specs."""
+    for spec in specs_to_capture:
+        if spec.env is None:
+            spec.env = {"XONSH_CAPTURE_ALWAYS": True}
+        else:
+            spec.env.setdefault("XONSH_CAPTURE_ALWAYS", True)
 
 
 def _shell_set_title(cmds):

tests/procs/test_specs.py
@@ -151,6 +151,25 @@ def test_capture_always(
 
 
 @skip_if_on_windows
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
+def test_callias_captured_redirect(xonsh_session, tmpdir):
+    @xonsh_session.aliases.register("a")
+    def _a(a, i, o, e):
+        print("print_stdout")
+        xonsh_session.subproc_captured_stdout(["echo", "cap_stdout"])
+        xonsh_session.subproc_captured_object(["echo", "cap_object"])
+        xonsh_session.subproc_captured_hiddenobject(["echo", "hiddenobject"])
+        xonsh_session.subproc_uncaptured(["echo", "uncaptured"])
+        print("print_error", file=e)
+
+    f = tmpdir / "capture.txt"
+    cmd = (["a", (">", str(f))],)
+    specs = cmds_to_specs(cmd, captured="hiddenobject")
+    _run_command_pipeline(specs, cmd).end()
+    assert f.read_text(encoding="utf-8") == "print_stdout\nhiddenobject\n"
+
+
+@skip_if_on_windows
 @pytest.mark.parametrize("captured", ["stdout", "object"])
 @pytest.mark.parametrize("interactive", [True, False])
 @pytest.mark.flaky(reruns=3, reruns_delay=2)