cb3496a1c5d0884ecd1c33b3992769c0585fda00
a <1@1.1>
2024-07-04 17:11:09 +0200

    deprecation warnings

---
+++ xonsh/lazyjson.py
@@ -0,0 +1,10 @@
+"""DEPRECATED: Use `xonsh.lib.lazyjson` instead of `xonsh.lazyjson`."""
+import warnings
+
+warnings.warn(
+        "Use `xonsh.lib.lazyjson` instead of `xonsh.lazyjson`.",
+        DeprecationWarning,
+        stacklevel=2
+    )
+
+from xonsh.lib.lazyjson import *
