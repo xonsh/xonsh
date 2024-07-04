9a9e00f5173e372c7aa07b0ff047cc2dfa3aba96
Noortheen Raja <jnoortheen@gmail.com>
2024-07-04 11:48:01 +0530

    test: use subtests when setup is required for parameters

--- tests/history/test_history_dummy.py
+++ tests/history/test_history_dummy.py
@@ -1,15 +1,15 @@
 """Tests the dummy history backend."""
 
-import pytest
-
-from xonsh.history.dummy import DummyHistory
 from xonsh.history.main import construct_history
 
 
-@pytest.mark.parametrize("backend", ["dummy", DummyHistory, DummyHistory()])
-def test_construct_history_str(xession, backend):
-    xession.env["XONSH_HISTORY_BACKEND"] = backend
-    assert isinstance(construct_history(), DummyHistory)
+def test_construct_history_str(xession, subtests):
+    from xonsh.history.dummy import DummyHistory
+
+    for backend in ["dummy", DummyHistory, DummyHistory()]:
+        with subtests.test(backend=backend):
+            xession.env["XONSH_HISTORY_BACKEND"] = backend
+            assert isinstance(construct_history(), DummyHistory)
 
 
 def test_ignore_regex_invalid(xession, capsys):
