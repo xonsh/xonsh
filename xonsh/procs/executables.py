3a9d53acb0dc91df2b771082d84b170f156a7b80
a <1@1.1>
2024-07-25 08:04:52 +0200

    prefix

--- xonsh/procs/executables.py
+++ xonsh/procs/executables.py
@@ -90,10 +90,7 @@ def locate_relative_path(name, env=None, check_executable=False, use_pathext=Fal
     """
     p = Path(name)
     if (
-        name.startswith("./")
-        or name.startswith("../")
-        or name.startswith(".\\")
-        or name.startswith("..\\")
+        name.startswith(("./", "../", ".\\", "..\\"))
         or p.is_absolute()
     ):
         possible_names = get_possible_names(p.name, env) if use_pathext else [p.name]
