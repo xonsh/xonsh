48b41cc89df8ea3c2e1008edbb6fb760461b9eb6
eugenesvk <eugenesvk@users.noreply.github.com>
2024-08-08 15:10:18 +0700

    add test_xonsh_win_path_dirs_to_list

--- tests/procs/test_executables.py
+++ tests/procs/test_executables.py
@@ -91,3 +91,45 @@ def test_locate_file(tmpdir, xession):
     with xession.env.swap(PATH=[str(bindir1), str(bindir2), str(bindir3)]):
         f = locate_file("findme")
         assert str(f) == str(file)
+
+def test_xonsh_win_path_dirs_to_list(tmpdir, xession):
+    # check that adding smaller dirs to a XONSH_WIN_PATH_DIRS_TO_LIST var to list them
+    # instead of checking for the existence of every file.pathext
+    # is faster
+    if not ON_WINDOWS:
+        return
+
+    from os import walk
+    # create a list of smaller dirs in PATH
+    short_path = 50 # exact threshold depends on the number of pathext, this is very ~
+    env = xession.env
+    env_path = env.get("PATH",[])
+    pathext = ['.COM','.EXE','.BAT','.CMD','.VBS','.VBE','.JS','.JSE','.WSF','.WSH','.MSC','.PY','.PYW','.XSH']
+    env['PATHEXT'] = pathext
+
+    if len(pathext) <= 2:
+        print(f"pathext is too short {len(pathext)} for direct listing of dirs to be faster")
+        return
+
+    xonsh_win_path_dirs_to_list = []
+    for path in env_path:
+        f = []
+        for (dirpath, dirnames, filenames) in walk(path):
+            f.extend(filenames)
+            break
+        if len(f) < short_path:
+            xonsh_win_path_dirs_to_list += [path.rstrip(os.path.sep)]
+
+    from time import monotonic_ns as ttime
+    from math import pow
+    ns = pow(10,9) # nanosecond, which 'monotonic_ns' are measured in
+
+    env['XONSH_WIN_PATH_DIRS_TO_LIST'] = None
+    t0 = ttime(); f = locate_executable("nothing"); t1 = ttime(); dur1 = (t1 - t0)/ns
+
+    env['XONSH_WIN_PATH_DIRS_TO_LIST'] = xonsh_win_path_dirs_to_list
+    t0 = ttime(); f = locate_executable("nothing"); t1 = ttime(); dur2 = (t1 - t0)/ns
+
+    print(f"{len(pathext)} file.exists checks; {len(xonsh_win_path_dirs_to_list)} paths with <{short_path} items from ∑{len(env_path)}")
+    print(f"🕐{{:.6f}}\t(file.ext)\n🕐{{:.6f}}\t(list small dirs)\t".format(dur1,dur2))
+    assert dur2 < 0.90 * dur1   # listing method should be noticeable faster
