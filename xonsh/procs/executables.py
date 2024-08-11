cc2c67de215a661540966a12c518715f9435bb40
eugenesvk <eugenesvk@users.noreply.github.com>
2024-08-12 01:57:44 +0700

    use missing use_perma_cache arg

--- xonsh/procs/executables.py
+++ xonsh/procs/executables.py
@@ -401,6 +401,7 @@ def locate_file_in_path_env(
     for path in paths:
         if (
             check_executable
+            and use_perma_cache
             and dir_cache_perma
             and path in dir_cache_perma
             and path in paths_cache
