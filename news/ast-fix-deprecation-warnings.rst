6fefc3c68d45f95080bbdd8129af2b66b6b8fb77
DoronZ <doron88@gmail.com>
2024-11-06 15:42:47 +0200

    parsers: fix deprecation warnings triggered on python3.13
    Close #5710

---
+++ news/ast-fix-deprecation-warnings.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* parsers: fix deprecation warnings triggered on python3.13
+
+**Security:**
+
+* <news item>
--- xonsh/parser.py
+++ xonsh/parser.py
@@ -6,7 +6,9 @@ from xonsh.platform import PYTHON_VERSION_INFO
 
 @lazyobject
 def Parser():
-    if PYTHON_VERSION_INFO > (3, 10):
+    if PYTHON_VERSION_INFO >= (3, 13):
+        from xonsh.parsers.v313 import Parser as p
+    elif PYTHON_VERSION_INFO > (3, 10):
         from xonsh.parsers.v310 import Parser as p
     elif PYTHON_VERSION_INFO > (3, 9):
         from xonsh.parsers.v39 import Parser as p
