045b92422e33c3da2a80b4f77aa956ecb49bc778
Joseph Montanaro <jfmonty2@gmail.com>
2025-01-27 06:50:24 -0500

    use sys.exc_info instead of sys.exception for version compatibility

--- xonsh/procs/pipelines.py
+++ xonsh/procs/pipelines.py
@@ -268,7 +268,7 @@ class CommandPipeline:
             # class is the real Popen
             PrevProcCloser(pipeline=self)
             task = None
-            if not isinstance(sys.exception(), SystemExit):
+            if not isinstance(sys.exc_info()[1], SystemExit):
                 task = xj.wait_for_active_job()
 
             if task is None or task["status"] != "stopped":
