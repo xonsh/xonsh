dbeb27688bdbbd5efd6ffcc246ce4181df4f66bc
Andy Kipp <anki-code@users.noreply.github.com>
2025-12-17 14:32:44 +0000

    docs: Update xonsh logo in the docs (#6004)
    ## For community
    ⬇️ **Please click the 👍 reaction instead of leaving a `+1` or 👍
    comment**
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -174,7 +174,7 @@ if not on_rtd:
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/ascii_conch_part_transparent_tight.png"
+html_logo = "_static/xonsh_terminal_icon_256.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
