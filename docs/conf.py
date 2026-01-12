29c97d5b1f411367500d54cdfe7678f30ef19c10
Andy Kipp <anki-code@users.noreply.github.com>
2026-01-12 21:53:08 +0600

    Update static file paths in conf.py
    Removed 'install' from html_static_path and added a new script to html_extra_path.

--- docs/conf.py
+++ docs/conf.py
@@ -184,9 +184,9 @@ html_favicon = "_static/magic_conch.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static", "install"]
+html_static_path = ["_static"]
 
-html_extra_path = ["_static/robots.txt"]
+html_extra_path = ["_static/robots.txt", "_static/install/mamba-install-xonsh.sh"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
