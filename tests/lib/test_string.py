45a5b8a58d813a9fcd80afa235177a66bc061f4a
lambda-abstraction <enikita332@gmail.com>
2026-02-18 21:06:24 +0300

    refactor: use pytest parametrize instead of for loop for case isolation

--- tests/lib/test_string.py
+++ tests/lib/test_string.py
@@ -1,8 +1,11 @@
+import pytest
+
 from xonsh.lib.string import commonprefix
 
 
-def test_commonprefix():
-    cases = [
+@pytest.mark.parametrize(
+    "strings, expected_prefix",
+    [
         ([], ""),
         (["ab", "cd"], ""),
         (["ls"], "ls"),
@@ -10,18 +13,19 @@ def test_commonprefix():
         (["a", "ab"], "a"),
         (["ab", "ab"], "ab"),
         (["python3", "python3.13"], "python3"),
-    ]
-    for strings, expected_prefix in cases:
-        prefix = commonprefix(strings)
-        length = len(prefix)
-        assert prefix == expected_prefix
-        # property tests (only make sense for non-empty strings):
-        if not strings:
-            continue
-        # order shouldnt matter
-        assert commonprefix(strings[::-1]) == prefix
-        # is a common prefix
-        assert all(string.startswith(prefix) for string in strings)
-        # since it is the longest, it should not be possible to extend it
-        if all(len(string) > length for string in strings):  # more characters left
-            assert len({string[length] for string in strings}) > 1  # not common
+    ],
+)
+def test_commonprefix(strings: list[str], expected_prefix: str):
+    prefix = commonprefix(strings)
+    length = len(prefix)
+    assert prefix == expected_prefix
+    # property tests (only make sense for non-empty strings):
+    if not strings:
+        return
+    # order shouldnt matter
+    assert commonprefix(strings[::-1]) == prefix
+    # is a common prefix
+    assert all(string.startswith(prefix) for string in strings)
+    # since it is the longest, it should not be possible to extend it
+    if all(len(string) > length for string in strings):  # more characters left
+        assert len({string[length] for string in strings}) > 1  # not common
