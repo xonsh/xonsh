4fd656cd0af3511cf01423123dff552b7c33d3e8
Andy Kipp <anki-code@users.noreply.github.com>
2026-03-11 21:27:55 +0600

    Make test_callable_alias_no_bad_file_descriptor stable
    Change interactive mode to False for test execution.

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -1698,7 +1698,7 @@ def test_callable_alias_no_bad_file_descriptor(test_code):
     """Test no exceptions during any kind of capturing of callable alias. See also #5631."""
 
     out, err, ret = run_xonsh(
-        test_code, interactive=True, single_command=True, timeout=60
+        test_code, interactive=False, single_command=True, timeout=60
     )
     assert ret == 0
     assert "Error" not in out
