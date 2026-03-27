9595289be5918a720ed9b6501e96ba60d26ae29a
SamLukeYes <samlukeyes123@gmail.com>
2026-03-27 10:58:36 +0800

    chore(nix): add nix packaging

---
+++ nix/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let 
+  callXonshPackage = pkgs.callPackage ./package.nix;
+in
+{
+  xonsh = callXonshPackage { };
+  xonsh-py311 = callXonshPackage { python3 = pkgs.python311; };
+  xonsh-py312 = callXonshPackage { python3 = pkgs.python312; };
+  xonsh-py313 = callXonshPackage { python3 = pkgs.python313; };
+  xonsh-py314 = callXonshPackage { python3 = pkgs.python314; };
+}
+
