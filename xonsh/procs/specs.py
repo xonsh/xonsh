643d630d0fb1519ab70ab216044b0962cd2d5fc7
Andy Kipp <anki-code@users.noreply.github.com>
2026-03-28 12:07:51 +0300

    fix(Callable aliases): Added exception with workaround in case of using explicit unthreadable callable alias in pipe (#6165)
    Closes #5317
    
    ### Before
    
    Hanging:
    
    ```xsh
    aliases['t'] = lambda: print(1)
    t | grep 1
    # 1  # Because there is no explicit `threadable` marker.
    
    @unthread t | grep 1
    # Hanging, because `t` is marked unthredable by decorator and is not supported.
    
    @aliases.register
    @@.imp.xonsh.tools.unthreadable
    def _unth():
        return '1'
    
    unth | head
    # Hanging, because `unth` is unthredable and is not supported.
    
    xontrib | grep core
    # Hanging, because xontrib is not thredable.
    ```
    
    ### After
    
    Added error message:
    
    ```xsh
    @aliases.register
    @@.imp.xonsh.tools.unthreadable
    def _unth():
        return '1'
    
    unth | head
    # Alias 'unth' is explicitly marked as unthreadable and is not supported in pipelines.
    # If it's really threadable try `@thread unth`.
    
    @thread unth | head
    # 1
    
    @thread xontrib | grep core
    # coreutils  not-loaded
    ```
    
    ## For community
    ⬇️ **Please click the 👍 reaction instead of leaving a `+1` or 👍
    comment**
    
    ---------
    
    Co-authored-by: Andy Kipp <123@321.123>

--- xonsh/procs/specs.py
+++ xonsh/procs/specs.py
@@ -1094,6 +1094,15 @@ def cmds_to_specs(cmds, captured=False, envs=None):
         )
         _set_specs_capture_always(specs_to_capture)
 
+    # Validate: unthreadable callable aliases cannot be used in pipelines
+    if len(specs) > 1:
+        for spec in specs:
+            if callable(spec.alias) and not spec.threadable:
+                raise xt.XonshError(
+                    f"Callable alias {spec.alias_name!r} is explicitly marked as unthreadable and is not supported in pipelines.\n"
+                    f"If it's really threadable try `@thread {spec.alias_name}`."
+                )
+
     _update_last_spec(specs[-1])
     return specs
 
