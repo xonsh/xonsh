5716bb3488cbe483ec00bdc611109719b6e738a3
Andy Kipp <123@321.123>
2026-04-02 00:04:07 +0600

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ master_doc = "contents"
 
 # General information about the project.
 project = "xonsh"
-copyright = '<a href="https://github.com/xonsh/xonsh/graphs/contributors">Xonsh Contributors</a>'
+copyright = 'Xonsh Contributors / https://github.com/xonsh/xonsh/graphs/contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
