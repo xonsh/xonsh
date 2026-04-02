c43ca2b4b502bdc98cc4081011667606c7eda016
Andy Kipp <123@321.123>
2026-04-02 23:18:07 +0600

    fix new line token

--- xonsh/parsers/tokenize.py
+++ xonsh/parsers/tokenize.py
@@ -934,7 +934,7 @@ def _tokenize(
                 if tolerant:
                     # return the partial string
                     yield TokenInfo(
-                        ERRORTOKEN, contstr, strstart, (lnum, end), contline + line
+                        ERRORTOKEN, contstr, strstart, (lnum, 0), contline + line
                     )
                     break
                 else:
