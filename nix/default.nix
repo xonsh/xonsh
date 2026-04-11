c0842239e62c4b61105b13d68260706712a8fdec
SamLukeYes <samlukeyes123@gmail.com>
2026-04-11 21:33:30 +0800

    chore(nix): init flake

--- nix/default.nix
+++ nix/default.nix
@@ -4,10 +4,10 @@ let
   callXonshPackage = pkgs.callPackage ./package.nix;
 in
 {
+  default = callXonshPackage { };
   xonsh = callXonshPackage { };
   xonsh-py311 = callXonshPackage { python3 = pkgs.python311; };
   xonsh-py312 = callXonshPackage { python3 = pkgs.python312; };
   xonsh-py313 = callXonshPackage { python3 = pkgs.python313; };
   xonsh-py314 = callXonshPackage { python3 = pkgs.python314; };
 }
-
